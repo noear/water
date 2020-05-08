ad363f4a1bcf28a4eb0df0c1b1b774fa34e9bb45
noear <noear@live.cn>
2020-05-08 15:25:14 +0800

    2.0.1-b3

--- waterapi/src/main/java/waterapp/WaterapiApp.java
+++ waterapi/src/main/java/waterapp/WaterapiApp.java
@@ -2,6 +2,9 @@ package waterapp;
 
 import org.noear.solon.XApp;
 import org.noear.solon.XUtil;
+import org.noear.solon.annotation.XMapping;
+import org.noear.solon.core.XMap;
+import org.noear.solon.extend.staticfiles.XStaticFiles;
 import org.noear.water.protocol.ProtocolHub;
 import org.noear.water.protocol.DefaultHeihei;
 import waterapp.wrap.LogStorerDb;
@@ -10,12 +13,27 @@ import waterapp.wrap.MessageQueueRedis;
 public class WaterapiApp {
 
 	public static void main(String[] args) {
-		 XApp.start(WaterapiApp.class, args, app->{
-			 Config.tryInit(app.port(), app.prop().getProp("water.dataSource"));
+		XMap argx = XMap.from(args);
 
-			 ProtocolHub.logStorer = LogStorerDb.singleton();
-			 ProtocolHub.messageQueue = MessageQueueRedis.singleton();
-			 ProtocolHub.heihei = DefaultHeihei.singleton();
-		 });
+		if (argx.containsKey("init")) {
+			XStaticFiles.instance().put(".sql", "text/sql");
+
+			XApp.start(WaterapiApp.class, argx, app -> {
+
+			});
+		} else {
+			//关掉静态文件支持
+			//
+			System.setProperty("org.noear.solon.extend.staticfiles.enabled","0");
+
+			XApp.start(WaterapiApp.class, argx, app -> {
+				Config.tryInit(app.port(), app.prop().getProp("water.dataSource"));
+
+				ProtocolHub.logStorer = LogStorerDb.singleton();
+				ProtocolHub.messageQueue = MessageQueueRedis.singleton();
+				ProtocolHub.heihei = DefaultHeihei.singleton();
+			});
+		}
+
 	}
 }
