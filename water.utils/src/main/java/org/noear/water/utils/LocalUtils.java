5aad1520a3f92bdddb980c849eb6d018f7415933
noear <noear@live.cn>
2020-05-28 10:25:00 +0800

    2.0.1-b5

--- water.utils/src/main/java/org/noear/water/utils/LocalUtils.java
+++ water.utils/src/main/java/org/noear/water/utils/LocalUtils.java
@@ -7,20 +7,34 @@ import java.util.Enumeration;
 public class LocalUtils {
     public static String getLocalIp(){
 
+        NetworkInterface neti = null;
         String host = null;
+
 
         try {
             Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
             Enumeration<InetAddress>      ee = null;
 
             while (en.hasMoreElements()) {
-                ee = en.nextElement().getInetAddresses();
-                while (ee.hasMoreElements()) {
-                    host = ee.nextElement().getHostAddress();
+                neti = en.nextElement();//网卡
 
-                    if(TextUtils.isEmpty(host) == false) {
-                        if (host.startsWith("192.") || host.startsWith("172.") || host.startsWith("10.")) {
-                            return host;
+                if(neti.getName() == null){
+                    continue;
+                }
+
+                //检查网卡名
+                if(neti.getName().startsWith("en") ||  neti.getName().startsWith("eth")) {
+                    //网卡绑定的地址
+                    ee = neti.getInetAddresses();
+
+                    while (ee.hasMoreElements()) {
+                        //地址
+                        host = ee.nextElement().getHostAddress();
+
+                        if (TextUtils.isEmpty(host) == false) {
+                            if (host.startsWith("192.") || host.startsWith("172.") || host.startsWith("10.")) {
+                                return host;
+                            }
                         }
                     }
                 }
