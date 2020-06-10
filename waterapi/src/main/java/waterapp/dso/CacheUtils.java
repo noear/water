1b400e047e184c012d49341b6fd701435c2563d3
noear <noear@live.cn>
2020-06-10 17:39:11 +0800

    2.0.1-b5

--- waterapi/src/main/java/waterapp/dso/CacheUtils.java
+++ waterapi/src/main/java/waterapp/dso/CacheUtils.java
@@ -4,7 +4,6 @@ import org.noear.weed.cache.ICacheServiceEx;
 import waterapp.Config;
 
 public class CacheUtils {
-    public static boolean isUsingCache = true;
     //public static ICacheService data = new LocalCache("data", 60 * 10);
     public static ICacheServiceEx data = Config
             .cfg("water_cache")
