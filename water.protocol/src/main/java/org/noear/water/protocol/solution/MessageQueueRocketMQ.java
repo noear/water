4dd15b24c43ef0d14061d1ddbe2ec9aa6ff823fc
noear <noear@live.cn>
2020-06-21 22:24:13 +0800

    dev-2.0.2

--- water.protocol/src/main/java/org/noear/water/protocol/solution/MessageQueueRocketMQ.java
+++ water.protocol/src/main/java/org/noear/water/protocol/solution/MessageQueueRocketMQ.java
@@ -1,16 +1,10 @@
 package org.noear.water.protocol.solution;
 
-import com.rabbitmq.client.ConnectionFactory;
-import org.noear.water.protocol.IMessageKeyBuilder;
 import org.noear.water.protocol.IMessageQueue;
 
-import java.util.Collection;
 
 public class MessageQueueRocketMQ implements IMessageQueue {
     String _queue_name;
-
-    ConnectionFactory factory;
-
 
     @Override
     public void push(String msg) {
