793fa427bb43b88a1e3532bae0455ab9080b58bd
noear <noear@live.cn>
2020-06-22 15:35:33 +0800

    2.0.1

--- demo/water-demo-api2/src/main/java/org/noear/water/demo_api/dso/db/UserDao.java
+++ demo/water-demo-api2/src/main/java/org/noear/water/demo_api/dso/db/UserDao.java
@@ -4,6 +4,6 @@ import org.noear.water.demo_api.model.BcfUserModel;
 import org.noear.weed.BaseMapper;
 import org.noear.weed.annotation.Db;
 
-@Db("water_bcf")
+@Db("water")
 public interface UserDao extends BaseMapper<BcfUserModel> {
 }
--- demo/water-demo-rpc-service-impl/src/main/java/org/noear/water/demo_rpc_service/dso/BcfUserRpcServiceImp.java
+++ demo/water-demo-rpc-service-impl/src/main/java/org/noear/water/demo_rpc_service/dso/BcfUserRpcServiceImp.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 @XBean(remoting = true)
 public class BcfUserRpcServiceImp implements BcfUserRpcService {
 
-    @Water("water/water_bcf")
+    @Water("water/water")
     DbContext bcfDb;
 
     @Override
