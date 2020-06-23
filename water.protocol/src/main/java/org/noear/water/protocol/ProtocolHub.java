2dc49713308c9c50c2b60131dd963460ec70fb15
noear <noear@live.cn>
2020-06-23 14:04:34 +0800

    2.0.2

--- water.protocol/src/main/java/org/noear/water/protocol/ProtocolHub.java
+++ water.protocol/src/main/java/org/noear/water/protocol/ProtocolHub.java
@@ -1,6 +1,14 @@
 package org.noear.water.protocol;
 
 import org.noear.water.protocol.solution.MessageKeyBuilderDefault;
+import org.noear.water.protocol.solution.MessageQueueRabbitMQ;
+import org.noear.water.protocol.solution.MessageQueueRedis;
+import org.noear.water.protocol.solution.MessageQueueRocketMQ;
+import org.noear.water.utils.RabbitMQX;
+import org.noear.water.utils.RedisX;
+import org.noear.water.utils.TextUtils;
+
+import java.util.Properties;
 
 /**
  * 协议中心
@@ -9,9 +17,40 @@ public final class ProtocolHub {
     public static ILogQuerier logQuerier;
     public static ILogStorer logStorer;
 
-    public static IMessageKeyBuilder messageKeyBuilder = new MessageKeyBuilderDefault();
     public static IMessageLock messageLock;
     public static IMessageQueue messageQueue;
 
     public static IHeihei heihei;
+
+    public static IMessageQueue createMessageQueue(Properties prop){
+        String type = prop.getProperty("queue.type","").toLowerCase();
+        String name = prop.getProperty("queue.name","").toLowerCase();
+
+        if(TextUtils.isEmpty(type) || TextUtils.isEmpty(type)){
+            throw new RuntimeException("ProtocolHub::There was an error in the input configuration");
+        }
+
+        if("redis".equals(type)){
+            //server
+            //user
+            //password
+            //db
+            return new MessageQueueRedis(name, new RedisX(prop));
+        }
+
+        if("rabbitmq".equals(type)){
+            //server
+            //user
+            //password
+            //virtualHost
+            return new MessageQueueRabbitMQ(name, new RabbitMQX(prop));
+        }
+
+        if("rocketmq".equals(type)){
+            //server
+            return new MessageQueueRocketMQ(name, prop);
+        }
+
+        throw new RuntimeException("ProtocolHub::There was an error in the input configuration");
+    }
 }
