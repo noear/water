df87656dca06730506810303df35dd8d0c220882
noear <noear@live.cn>
2020-07-02 12:39:47 +0800

    2.0.2-完成日志管道模式

--- water.client/src/main/java/org/noear/water/dso/LogPipeline.java
+++ water.client/src/main/java/org/noear/water/dso/LogPipeline.java
@@ -2,19 +2,14 @@ package org.noear.water.dso;
 
 import org.noear.water.WaterClient;
 import org.noear.water.log.LogEvent;
-import org.noear.water.utils.TaskUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import org.noear.water.utils.EventPipeline;
 
 /**
  * 写入时，先写到队列
  * 提交时，每次提交100条；消费完后暂停1秒
  *
  * */
-public class LogPipeline implements TaskUtils.ITask {
+public class LogPipeline extends EventPipeline<LogEvent> {
     private static LogPipeline singleton = new LogPipeline();
 
     public static LogPipeline singleton() {
@@ -22,86 +17,8 @@ public class LogPipeline implements TaskUtils.ITask {
     }
 
     private LogPipeline() {
-        TaskUtils.run(this);
-    }
-
-    //
-    //
-    //
-
-    Queue<LogEvent> queueLocal = new LinkedBlockingQueue<>();
-
-    public void add(LogEvent event) {
-        try {
-            queueLocal.add(event);
-        } catch (Exception ex) {
-            //不打印
-        }
-    }
-
-
-    private long interval = 500;
-    private long interval_min = 100;
-
-    private int packetSize = 100; //必须大于100
-    private int packetSize_min = 100; //必须大于100
-
-    /**
-     * 获取任务间隔时间
-     * */
-    @Override
-    public long getInterval() {
-        return interval;
-    }
-
-    public void setInterval(long interval) {
-        if (interval >= interval_min) {
-            this.interval = interval;
-        }
-    }
-
-    /**
-     * 设置管道包大小
-     * */
-    public void setPacketSize(int packetSize) {
-        if (packetSize >= packetSize_min) {
-            this.packetSize = packetSize;
-        }
-    }
-
-    @Override
-    public void exec() throws Exception {
-
-        while (true) {
-            List<LogEvent> list = new ArrayList<>(packetSize);
-
-            collectDo(list);
-
-            if (list.size() > 0) {
-                WaterClient.Log.appendAll(list, true);
-            } else {
-                break;
-            }
-        }
-    }
-
-    private void collectDo(List<LogEvent> list) {
-        //收集最多100条日志
-        //
-        int count = 0;
-        while (true) {
-            LogEvent log = queueLocal.poll();
-
-            if (log == null) {
-                break;
-            } else {
-                list.add(log);
-                count++;
-
-                if (count == packetSize) {
-                    break;
-                }
-            }
-        }
+        super((list) -> {
+            WaterClient.Log.appendAll(list, true);
+        });
     }
 }
---
+++ water.utils/src/main/java/org/noear/water/utils/EventPipeline.java
@@ -0,0 +1,113 @@
+package org.noear.water.utils;
+
+import org.noear.water.utils.ext.Act1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 写入时，先写到队列
+ * 提交时，每次提交100条；消费完后暂停1秒
+ *
+ * */
+public class EventPipeline<Event> implements TaskUtils.ITask {
+    private long interval = 500;
+    private long interval_min = 100;
+
+    private int packetSize = 100; //必须大于100
+    private int packetSize_min = 100; //必须大于100
+
+    private Act1<List<Event>> handler;
+
+    public EventPipeline(Act1<List<Event>> handler) {
+        this.handler = handler;
+
+        TaskUtils.run(this);
+    }
+
+    public EventPipeline(long interval,int packetSize, Act1<List<Event>> handler) {
+        this.handler = handler;
+
+        setInterval(interval);
+        setPacketSize(packetSize);
+        TaskUtils.run(this);
+    }
+
+    //
+    //
+    //
+
+    Queue<Event> queueLocal = new LinkedBlockingQueue<>();
+
+    public void add(Event event) {
+        try {
+            queueLocal.add(event);
+        } catch (Exception ex) {
+            //不打印
+        }
+    }
+
+
+
+
+    /**
+     * 获取任务间隔时间
+     * */
+    @Override
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        if (interval >= interval_min) {
+            this.interval = interval;
+        }
+    }
+
+    /**
+     * 设置管道包大小
+     * */
+    public void setPacketSize(int packetSize) {
+        if (packetSize >= packetSize_min) {
+            this.packetSize = packetSize;
+        }
+    }
+
+    @Override
+    public void exec() throws Exception {
+
+        while (true) {
+            List<Event> list = new ArrayList<>(packetSize);
+
+            collectDo(list);
+
+            if (list.size() > 0) {
+                handler.run(list);
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void collectDo(List<Event> list) {
+        //收集最多100条日志
+        //
+        int count = 0;
+        while (true) {
+            Event event = queueLocal.poll();
+
+            if (event == null) {
+                break;
+            } else {
+                list.add(event);
+                count++;
+
+                if (count == packetSize) {
+                    break;
+                }
+            }
+        }
+    }
+}
