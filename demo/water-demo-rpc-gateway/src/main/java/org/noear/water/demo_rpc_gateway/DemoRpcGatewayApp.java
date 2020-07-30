9ea0de29ba6168247368634d5aa247245cad8542
noear <noear@live.cn>
2020-07-30 18:26:26 +0800

    2.0.4

--- demo/water-demo-rpc-gateway/src/main/java/org/noear/water/demo_rpc_gateway/DemoRpcGatewayApp.java
+++ demo/water-demo-rpc-gateway/src/main/java/org/noear/water/demo_rpc_gateway/DemoRpcGatewayApp.java
@@ -2,11 +2,11 @@ package org.noear.water.demo_rpc_gateway;
 
 import org.noear.solon.XApp;
 import org.noear.solonclient.XProxy;
-import org.noear.solonclient.serializer.SnackSerializer;
+import org.noear.solonclient.serializer.SnackSerializerD;
 
 public class DemoRpcGatewayApp {
     public static void main(String[] args) {
-        XProxy.defaultSerializer = new SnackSerializer();
+        XProxy.defaultDeserializer = SnackSerializerD.instance;
 
         XApp.start(DemoRpcGatewayApp.class, args);
     }
