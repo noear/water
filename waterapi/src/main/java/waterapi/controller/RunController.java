493afa7f2f0084dd03b0d83e3662132f6064b95c
noear <noear@live.cn>
2020-08-07 11:25:11 +0800

    2.0.4

--- waterapi/src/main/java/waterapi/controller/RunController.java
+++ waterapi/src/main/java/waterapi/controller/RunController.java
@@ -14,82 +14,71 @@ import java.util.Map;
 /**
  * Created by noear on 2017/7/18.
  */
-@XMapping("/run/**")
+@XMapping("/run/")
 @XController
-public class RunController implements XHandler {
+public class RunController {
 
-    @Override
-    public void handle(XContext c) throws Exception {
-        String path = c.path();
+    @XMapping("check/")
+    public String run_check(XContext ctx) throws Exception {
+        DbWaterCfgApi.loadWhitelist(); //检测服务时，就会进行白名单刷新
 
-        switch (path) {
-            case "/run/check/": {
-                DbWaterCfgApi.loadWhitelist(); //检测服务时，就会进行白名单刷新
+        ONode data = new ONode();
 
-                ONode data = new ONode();
+        data.set("code", 1);
 
-                data.set("code", 1);
+        return data.toJson();
+    }
 
-                c.output(data.toJson());
-                break;
-            }
+    @XMapping("push/")
+    public void run_push(XContext ctx) {
+        new CMD_run_push().exec(ctx);
+    }
 
-            case "/run/push/": {
-                new CMD_run_push().exec(c);
-                break;
-            }
-
-            case "/run/whitelist/reload/": {
-                ONode data = new ONode();
-                try {
-                    DbWaterCfgApi.loadWhitelist();
-                    data.set("code", 1);
-                    data.set("msg", "ok");
-                } catch (Exception ex) {
-                    data.set("code", 0);
-                    data.set("msg", ex.getMessage());
-                }
-
-                c.output(data.toJson());
-                break;
-            }
-
-            case "/run/whitelist/check/": {
-                String tags = c.param("tags","");
-                String type = c.param("type","");
-                String value = c.param("value","");
-
-                if(DbWaterCfgApi.isWhitelist(tags, type,value)){
-                    c.output("OK");
-                }else{
-                    c.output(  value + ",not is whitelist!");
-                }
-                break;
-            }
-
-
-            case "/run/check/http/": {
-                ONode data = new ONode();
-
-                Map<String, String> map = c.headerMap();
-                if (map != null) {
-                    map.forEach((k, v) -> {
-                        data.set(k, v);
-                    });
-                }
-
-                data.set("__RemoteIp", c.header("RemoteIp"));
-                data.set("__X-Forwarded-For", c.header("X-Forwarded-For"));
-                data.set("__Proxy-Client-IP", c.header("Proxy-Client-IP"));
-                data.set("__WL-Proxy-Client-IP", c.header("WL-Proxy-Client-IP"));
-                data.set("__getRemoteAddr", c.ip());
-
-                c.output(data.toJson());
-                break;
-            }
-
-            default:
-                break;
+    @XMapping("whitelist/reload/")
+    public String run_whitelist_reload() {
+        ONode data = new ONode();
+        try {
+            DbWaterCfgApi.loadWhitelist();
+            data.set("code", 1);
+            data.set("msg", "ok");
+        } catch (Exception ex) {
+            data.set("code", 0);
+            data.set("msg", ex.getMessage());
         }
+
+        return data.toJson();
+    }
+
+    @XMapping("whitelist/check/")
+    public String run_whitelist_check(XContext ctx) throws Exception {
+        String tags = ctx.param("tags", "");
+        String type = ctx.param("type", "");
+        String value = ctx.param("value", "");
+
+        if (DbWaterCfgApi.isWhitelist(tags, type, value)) {
+            return ("OK");
+        } else {
+            return (value + ",not is whitelist!");
+        }
+    }
+
+    @XMapping("check/http/")
+    public String run_check_http(XContext ctx) {
+        ONode data = new ONode();
+
+        Map<String, String> map = ctx.headerMap();
+        if (map != null) {
+            map.forEach((k, v) -> {
+                data.set(k, v);
+            });
+        }
+
+        data.set("__RemoteIp", ctx.header("RemoteIp"));
+        data.set("__X-Forwarded-For", ctx.header("X-Forwarded-For"));
+        data.set("__Proxy-Client-IP", ctx.header("Proxy-Client-IP"));
+        data.set("__WL-Proxy-Client-IP", ctx.header("WL-Proxy-Client-IP"));
+        data.set("__getRemoteAddr", ctx.ip());
+
+        return data.toJson();
     }
 }
