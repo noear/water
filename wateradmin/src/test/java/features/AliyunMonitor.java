5964e27d264d9c8c71784e71240ee4e9d7914b89
noear <noear@live.cn>
2020-08-27 15:58:43 +0800

    2.0.4

---
+++ wateradmin/src/test/java/features/AliyunMonitor.java
@@ -0,0 +1,16 @@
+package features;
+
+import org.junit.Test;
+
+public class AliyunMonitor {
+    @Test
+    public void test(){
+
+    }
+
+    private String aliyunApi(String iaasType,String iaasKey){
+        String url = "https://ecs.aliyuncs.com/?Action=CreateSnapshot";
+
+        return "";
+    }
+}
