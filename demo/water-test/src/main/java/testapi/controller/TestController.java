8f6e86f3efc30e3fb0dfa7438d1c6ecae591599b
noear <noear@live.cn>
2020-09-21 15:00:53 +0800

    2.0.4

--- demo/water-test/src/main/java/testapi/controller/TestController.java
+++ demo/water-test/src/main/java/testapi/controller/TestController.java
@@ -1,13 +1,18 @@
 package testapi.controller;
 
+import org.noear.solon.XUtil;
 import org.noear.solon.annotation.XController;
 import org.noear.solon.annotation.XMapping;
+import org.noear.water.WaterClient;
 
 
 @XController
 public class TestController {
     @XMapping("/")
-    public String home() {
+    public String home(String msg) throws Exception{
+        if(XUtil.isNotEmpty(msg)){
+            WaterClient.Message.sendMessage("water.test.hello", msg);
+        }
         return "OK";
     }
 }
--- demo/water-test/src/main/java/testapi/controller/TestMessage.java
+++ demo/water-test/src/main/java/testapi/controller/TestMessage.java
@@ -5,11 +5,11 @@ import org.noear.water.annotation.WaterMessage;
 import org.noear.water.dso.MessageHandler;
 import org.noear.water.model.MessageM;
 
-@WaterMessage("test.hello")
+@WaterMessage("water.test.hello")
 public class TestMessage implements MessageHandler {
     @Override
     public boolean handler(MessageM msg) throws Throwable {
-        WaterClient.Message.sendMessage("test.hello", "Hello");
+        WaterClient.Message.sendMessage("water.test.hello", msg.message);
         return true;
     }
 }
