6d5046a159bc88d80c499901c875aec9b57c4cc4
noear <noear@live.cn>
2020-09-21 15:09:58 +0800

    2.0.4

--- demo/water-test/src/main/java/testapi/controller/TestMessage.java
+++ demo/water-test/src/main/java/testapi/controller/TestMessage.java
@@ -1,15 +1,18 @@
 package testapi.controller;
 
+import org.noear.solon.XUtil;
 import org.noear.water.WaterClient;
 import org.noear.water.annotation.WaterMessage;
 import org.noear.water.dso.MessageHandler;
 import org.noear.water.model.MessageM;
+import org.noear.water.utils.Datetime;
 
 @WaterMessage("water.test.hello")
 public class TestMessage implements MessageHandler {
     @Override
     public boolean handler(MessageM msg) throws Throwable {
-        WaterClient.Message.sendMessage("water.test.hello", msg.message);
+        Datetime dt = Datetime.Now().addSecond(10);
+        WaterClient.Message.sendMessage(XUtil.guid(),"water.test.hello", msg.message, dt.getFulltime());
         return true;
     }
 }
