7d049a07bf79f807619a8a4099048e3488152b34
noear <noear@live.cn>
2020-12-28 11:53:42 +0800

    2.0.5

--- demo/water-test-springboot/src/main/java/testapi2/TestApi2App.java
+++ demo/water-test-springboot/src/main/java/testapi2/TestApi2App.java
@@ -1,30 +1,18 @@
 package testapi2;
 
 import org.noear.solon.Solon;
-import org.noear.solon.extend.servlet.SolonServletFilter;
+import org.noear.solon.extend.springboot.SpringBootLinkSolon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author noear 2020/12/28 created
  */
-@Configuration
+@SpringBootLinkSolon
 @SpringBootApplication
 public class TestApi2App {
     public static void main(String[] args) {
         Solon.start(TestApi2App.class, args);
         SpringApplication.run(TestApi2App.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean servletRegistrationBean() {
-
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new SolonServletFilter());
-
-        return registration;
     }
 }
