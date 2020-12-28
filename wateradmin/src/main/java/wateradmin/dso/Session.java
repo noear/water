b3f1744238c9c083813430b85e06503a9310f400
noear <noear@live.cn>
2020-12-28 21:41:21 +0800

    2.0.5

--- wateradmin/src/main/java/wateradmin/dso/Session.java
+++ wateradmin/src/main/java/wateradmin/dso/Session.java
@@ -2,11 +2,11 @@ package wateradmin.dso;
 
 
 import org.noear.bcf.BcfClient;
-import org.noear.bcf.XSessionBcf;
+import org.noear.bcf.BcfSessionBase;
 import org.noear.bcf.models.BcfUserModel;
 import wateradmin.Config;
 
-public final class Session extends XSessionBcf {
+public final class Session extends BcfSessionBase {
     private static final Session _current = new Session();
     public static Session current() {
         return _current;
