15bdd38953db7bd56e9d6581fc859b2a996c1471
noear <noear@live.cn>
2021-01-16 18:12:38 +0800

    2.0.10

--- water.client-springboot-starter/src/main/java/org/noear/water/integration/springboot/AutoConfiguration.java
+++ water.client-springboot-starter/src/main/java/org/noear/water/integration/springboot/AutoConfiguration.java
@@ -4,13 +4,9 @@ import org.noear.solon.extend.springboot.SpringBootLinkSolon;
 import org.noear.water.annotation.Water;
 import org.noear.water.integration.solon.WaterBeanInjector;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
-
-import java.beans.PropertyDescriptor;
 
 /**
  * @author noear 2021/1/6 created
