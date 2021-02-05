afaaa24470446bbded29941b8cf3795d5e9e5328
noear <noear@live.cn>
2021-02-05 15:24:36 +0800

    2.0.17

--- water.protocol/src/main/java/org/noear/water/protocol/MessageSource.java
+++ water.protocol/src/main/java/org/noear/water/protocol/MessageSource.java
@@ -5,7 +5,6 @@ import org.noear.water.protocol.model.message.MessageModel;
 import org.noear.water.protocol.model.message.MessageState;
 import org.noear.water.protocol.model.message.SubscriberModel;
 
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
@@ -16,11 +15,11 @@ public interface MessageSource {
     //
     //for api
     //
-    boolean hasMessage(String msg_key) throws SQLException;
-    void setMessageAsCancel(String msg_key) throws SQLException;
-    void setMessageAsSucceed(String msg_key) throws SQLException;
-    void setDistributionAsCancel(String msg_key, String subscriber_key) throws SQLException;
-    void setDistributionAsSucceed(String msg_key, String subscriber_key) throws SQLException;
+    boolean hasMessage(String msg_key) throws Exception;
+    void setMessageAsCancel(String msg_key) throws Exception;
+    void setMessageAsSucceed(String msg_key) throws Exception;
+    void setDistributionAsCancel(String msg_key, String subscriber_key) throws Exception;
+    void setDistributionAsSucceed(String msg_key, String subscriber_key) throws Exception;
     long addMessage(int topic_id, String topic_name, String content) throws Exception;
     long addMessage(String msg_key, String trace_id, String tags, int topic_id,String topic_name, String content, Date plan_time) throws Exception;
 
@@ -28,41 +27,41 @@ public interface MessageSource {
     //
     //for sev
     //
-    List<Long> getMessageListOfPending(int rows, long dist_nexttime) throws SQLException;
-    MessageModel getMessageOfPending(long msg_id) throws SQLException;
+    List<Long> getMessageListOfPending(int rows, long dist_nexttime) throws Exception;
+    MessageModel getMessageOfPending(long msg_id) throws Exception;
     void setMessageRouteState(MessageModel msg, boolean dist_routed);
     boolean setMessageState(MessageModel msg, MessageState state);
     boolean setMessageState(MessageModel msg, MessageState state, long dist_nexttime);
     boolean setMessageRepet(MessageModel msg, MessageState state);
 
-    void addDistributionNoLock(MessageModel msg, SubscriberModel subs) throws SQLException;
-    List<DistributionModel> getDistributionListByMsg(long msg_id) throws SQLException;
+    void addDistributionNoLock(MessageModel msg, SubscriberModel subs) throws Exception;
+    List<DistributionModel> getDistributionListByMsg(long msg_id) throws Exception;
     boolean setDistributionState(MessageModel msg, DistributionModel dist, MessageState state);
 
 
     //
     //for admin
     //
-    MessageModel getMessageByKey(String msg_key) throws SQLException;
-    MessageModel getMessageById(long msg_id) throws SQLException;
-    List<MessageModel> getMessageList(int dist_count, int topic_id) throws SQLException;
-    List<MessageModel> getMessageList(int _m, String key) throws SQLException;
-//    int deleteMsg(int state) throws SQLException;
+    MessageModel getMessageByKey(String msg_key) throws Exception;
+    MessageModel getMessageById(long msg_id) throws Exception;
+    List<MessageModel> getMessageList(int dist_count, int topic_id) throws Exception;
+    List<MessageModel> getMessageList(int _m, String key) throws Exception;
+//    int deleteMsg(int state) throws Exception;
     /**
      * 获取派发记录
      * */
-    List<DistributionModel> getDistributionListByMsgIds(List<Object> ids) throws SQLException;
+    List<DistributionModel> getDistributionListByMsgIds(List<Object> ids) throws Exception;
     /**
      * 修复派发的订阅地址
      * */
-    boolean setDistributionReceiveUrl(long dist_id, String receive_url) throws SQLException;
+    boolean setDistributionReceiveUrl(long dist_id, String receive_url) throws Exception;
 
     /**
      * 设置消息为预备状态
      * */
-    boolean setMessageAsPending(List<Object> ids) throws SQLException;
+    boolean setMessageAsPending(List<Object> ids) throws Exception;
     /**
      * 设置消息为取消状态
      * */
-    boolean setMessageAsCancel(List<Object> ids) throws SQLException;
+    boolean setMessageAsCancel(List<Object> ids) throws Exception;
 }
