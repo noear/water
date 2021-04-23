95dce327ee8b0ddb42cc177ace1c09eba2374793
noear <noear@live.cn>
2021-04-23 18:48:30 +0800

    2.0.27

--- _db_log/water_msg.sql
+++ _db_log/water_msg.sql
@@ -15,3 +15,8 @@ ALTER TABLE `water_msg_message_all`
 
 update  water_msg_message set last_date = log_date where last_date=0;
 update  water_msg_message_all set last_date = log_date where last_date=0;
+
+
+-- 2021.04.23
+ALTER TABLE `water_msg_subscriber`
+    ADD INDEX `IX_receive_url`(`receive_url`(40)) USING BTREE;
