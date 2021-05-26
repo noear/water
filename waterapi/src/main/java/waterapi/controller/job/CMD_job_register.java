ee43a666c314db23ebb017968cc927b2d0afb812
noear <noear@live.cn>
2021-05-26 21:31:25 +0800

    2.1.2

--- waterapi/src/main/java/waterapi/controller/job/CMD_job_register.java
+++ waterapi/src/main/java/waterapi/controller/job/CMD_job_register.java
@@ -3,11 +3,11 @@ package waterapi.controller.job;
 import org.noear.snack.ONode;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.Result;
 import org.noear.solon.extend.validation.annotation.NotEmpty;
 import org.noear.solon.extend.validation.annotation.Whitelist;
 import waterapi.controller.UapiBase;
+import waterapi.dso.CacheUtils;
 import waterapi.dso.db.DbPassApi;
 import waterapi.dso.interceptor.Logging;
 
@@ -27,15 +27,26 @@ public class CMD_job_register extends UapiBase {
      */
     @NotEmpty({"tag", "service", "jobs"})
     @Mapping("/job/register/")
-    public Result cmd_exec(Context ctx, String tag, String service, String jobs) throws Exception {
+    public Result cmd_exec(String tag, String service, String jobs) throws Exception {
         if (jobs.startsWith("{") == false) {
             return Result.failure();
         }
 
         Map<String, String> jobMap = ONode.deserialize(jobs, Map.class);
+        StringBuilder buf = new StringBuilder(200);
 
         for (Map.Entry<String, String> kv : jobMap.entrySet()) {
-            DbPassApi.addJob(tag, service, kv.getKey(), kv.getValue());
+            buf.setLength(0);
+            String cacheKey = buf.append("job.register:")
+                    .append(tag).append(":")
+                    .append(service).append(":").append(kv.getKey())
+                    .toString();
+
+            CacheUtils.data.getBy(cacheKey, (cu) -> {
+                DbPassApi.addJob(tag, service, kv.getKey(), kv.getValue());
+                return "";
+            });
+
         }
 
         return Result.succeed();
