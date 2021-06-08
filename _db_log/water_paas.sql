3a7e09601a42ebc6c59f41f0c00c592ff289d0ce
noear <noear@live.cn>
2021-06-08 11:31:04 +0800

    2.1.6

--- _db_log/water_paas.sql
+++ _db_log/water_paas.sql
@@ -4,7 +4,7 @@ ALTER TABLE `paas_file`
     ADD COLUMN `use_whitelist` varchar(40) NULL COMMENT '安全名单' AFTER `update_fulltime`;
 
 
-
+-- 2021.06.08
 ALTER TABLE `paas_file`
     MODIFY COLUMN `plan_begin_time` timestamp NULL DEFAULT NULL COMMENT '计划开始执行时间' AFTER `plan_state`,
     MODIFY COLUMN `plan_last_time` timestamp NULL DEFAULT NULL COMMENT '计划最后执行时间' AFTER `plan_begin_time`,
