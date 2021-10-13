86c39f3cf49426601cbfb3a6551dfd5b9f42124d
noear <noear@live.cn>
2021-10-13 19:04:31 +0800

    2.2.13

--- _db_log/water_log.sql
+++ _db_log/water_log.sql
@@ -1,57 +1,57 @@
 
 -- 2021.10.13
 ALTER TABLE `sponge_log_admin`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `sponge_log_rock`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `sponge_log_track`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_admin`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_api`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_bcf`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_etl`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_heihei`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 
 ALTER TABLE `water_log_msg`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_paas`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_raas`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_sev`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 ALTER TABLE `water_log_upstream`
-    ADD COLUMN `app_name` varchar(100) NULL COMMENT '应用名' AFTER `summary`,
-    ADD INDEX `IX_app_name`(`app_name`) USING BTREE;
+    ADD COLUMN `service` varchar(100) NULL COMMENT '服务' AFTER `summary`,
+    ADD INDEX `IX_service`(`service`) USING BTREE;
 
 
 
