6b3ea880ba78390fa944132bd7447c7c0a96044b
noear <noear@live.cn>
2021-10-24 08:46:50 +0800

    2.2.17

--- water.utils/src/main/java/org/noear/water/log/LogEvent.java
+++ water.utils/src/main/java/org/noear/water/log/LogEvent.java
@@ -12,8 +12,10 @@ public class LogEvent {
     public String tag1;
     public String tag2;
     public String tag3;
+    public String tag4;
     public String summary;
     public String content;
+    public String app_name;
     public String class_name;
     public String thread_name;
     public String from;
