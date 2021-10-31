1f826b1ddc155e374bdb716d35d91e5279eb64a2
noear <noear@live.cn>
2021-11-01 05:24:30 +0800

    2.2.19

---
+++ water.protocol/src/main/java/org/noear/water/protocol/solution/MessageQueueLocal.java
@@ -0,0 +1,47 @@
+package org.noear.water.protocol.solution;
+
+import org.noear.water.protocol.MessageQueue;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
+
+/**
+ * @author noear 2021/11/1 created
+ */
+public class MessageQueueLocal implements MessageQueue {
+    Queue<String> queueLocal = new LinkedBlockingQueue<>();
+    @Override
+    public boolean push(String msg_id) {
+        queueLocal.add(msg_id);
+        return true;
+    }
+
+    @Override
+    public String poll() {
+        return queueLocal.poll();
+    }
+
+    @Override
+    public void pollGet(Consumer<String> callback) {
+        while (true) {
+            String msg = queueLocal.poll();
+
+            if (msg == null) {
+                break;
+            }
+
+            callback.accept(msg);
+        }
+    }
+
+    @Override
+    public long count() {
+        return queueLocal.size();
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
