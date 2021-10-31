07d9d1857ddf6e3b3a65653ad67f33c5074a2115
noear <noear@live.cn>
2021-10-31 14:11:44 +0800

    2.2.18

--- watersetup/src/main/java/watersetup/dso/BcfTagChecker.java
+++ watersetup/src/main/java/watersetup/dso/BcfTagChecker.java
@@ -9,24 +9,19 @@ import java.util.List;
 //不能用静态函数
 public class BcfTagChecker {
 
-    public static <T> void filterWaterTag(List<T> list, Fun1<String,T> getter) throws SQLException{
-        for(int i=0,len=list.size(); i<len; i++){
+    public static <T> void filterWaterTag(List<T> list, Fun1<String, T> getter) throws SQLException {
+        for (int i = 0, len = list.size(); i < len; i++) {
             String tag = getter.run(list.get(i));
 
-            if(TextUtils.isEmpty(tag)){
-                list.remove(i);
-                i--;
-                len--;
-            }else {
-                tag = tag.split("\\.|_")[0];
-
-                if (tag.startsWith("water") == false) {
-                    list.remove(i);
-                    i--;
-                    len--;
+            if (TextUtils.isNotEmpty(tag)) {
+                if (tag.startsWith("_") || tag.startsWith("water")) {
+                    continue;
                 }
             }
+
+            list.remove(i);
+            i--;
+            len--;
         }
     }
-
 }
