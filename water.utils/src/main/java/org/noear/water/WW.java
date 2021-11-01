f84bdae76f5f0159b9d5106de285ec31a546d3d4
noear <noear@live.cn>
2021-11-01 18:02:49 +0800

    2.3.0

--- water.utils/src/main/java/org/noear/water/WW.java
+++ water.utils/src/main/java/org/noear/water/WW.java
@@ -57,6 +57,7 @@ public class WW {
     public static final String logger_water_log_raas = "water_log_raas";
     public static final String logger_water_log_admin = "water_log_admin";
     public static final String logger_water_log_heihei = "water_log_heihei";
+    public static final String logger_water_log_upstream = "water_log_upstream";
 
     public static final String logger_rubber_log_request = "rubber_log_request";
 
