ae4741806b6bbec383779673888bfe7a45025f15
noear <noear@live.cn>
2021-11-02 23:29:45 +0800

    2.3.0

--- watersetup/src/main/java/watersetup/controller/init/Init2WaterRedisController.java
+++ watersetup/src/main/java/watersetup/controller/init/Init2WaterRedisController.java
@@ -8,6 +8,7 @@ import org.noear.solon.annotation.Post;
 import org.noear.solon.core.event.EventBus;
 import org.noear.solon.core.handle.Result;
 import org.noear.water.WW;
+import org.noear.water.model.ConfigM;
 import watersetup.Config;
 import watersetup.dso.db.DbWaterCfgApi;
 
@@ -26,8 +27,9 @@ public class Init2WaterRedisController {
             return Result.failure("未连接数据库，刷新再试...");
         }
 
+        Properties props;
         try {
-            Properties props = Utils.buildProperties(config);
+            props = Utils.buildProperties(config);
             RedisClient redisClient = new RedisClient(props, 0);
             redisClient.open(s -> {
                 s.key("test").get();
@@ -37,11 +39,29 @@ public class Init2WaterRedisController {
             return Result.failure("连接失败");
         }
 
+        Properties waterPro = Config.getCfg(WW.water, WW.water).getProp();
+
         String cacheCfg = "driverType=redis\n" + config + "\ndb=9";
+
+        StringBuilder bcfCfg = new StringBuilder();
+        bcfCfg.append("bcf.cache.driverType=").append("redis").append("\n");
+        bcfCfg.append("bcf.cache.server=").append(props.getProperty("server")).append("\n");
+        bcfCfg.append("bcf.cache.password=").append(props.getProperty("password")).append("\n");
+        bcfCfg.append("bcf.cache.db=").append("9").append("\n");
+        bcfCfg.append("\n");
+
+        bcfCfg.append("bcf.db.schema=").append(waterPro.getProperty("schema")).append("\n");
+        bcfCfg.append("bcf.db.url=").append(waterPro.getProperty("url")).append("\n");
+        bcfCfg.append("bcf.db.password=").append(waterPro.getProperty("password")).append("\n");
+        bcfCfg.append("bcf.db.username=").append(waterPro.getProperty("username")).append("\n");
+        bcfCfg.append("\n");
+        bcfCfg.append("server.session.state.domain=").append("water.noear.org").append("\n");
+        bcfCfg.append("server.session.timeout=").append("7200").append("\n");
 
         //更新配置
         DbWaterCfgApi.updConfig(WW.water, WW.water_redis, config);
         DbWaterCfgApi.updConfig(WW.water, WW.water_cache, cacheCfg);
+        DbWaterCfgApi.updConfig("water_bcf", "bcf.yml", cacheCfg);
         DbWaterCfgApi.updConfig(WW.water, Config.water_setup_step, "2");
 
         //2.
