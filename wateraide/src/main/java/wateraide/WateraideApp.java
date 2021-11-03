4a38a1c1681f9357c42e14f836e3284a56dcf7d1
noear <noear@live.cn>
2021-11-03 18:07:11 +0800

    2.3.0

--- wateraide/src/main/java/wateraide/WateraideApp.java
+++ wateraide/src/main/java/wateraide/WateraideApp.java
@@ -8,6 +8,7 @@ public class WateraideApp {
 
     public static void main(String[] args) {
         NvMap argx = NvMap.from(args);
+        argx.put("debug","1");
 
         Solon.start(WateraideApp.class, argx, x -> {
             //加载环境变量(支持弹性容器设置的环境)
