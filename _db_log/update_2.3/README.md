e2d2c9febb59770617e7800fb0f1a518b5e0dde3
noear <noear@live.cn>
2021-11-03 14:06:34 +0800

    2.3.0

--- _db_log/update_2.3/README.md
+++ _db_log/update_2.3/README.md
@@ -69,8 +69,9 @@ queue.password=L8vcQgep
 queue.db=3
 ```
 
+9. 添加 配置管理/消息配置: water/default 数据源为空
 
-9. 更新 water 服务包，然后重新
+10. 更新 water 服务包，然后重新
 
 注：更新后，water 部份定时任务运失败；把那失败的任务删掉，导入 water_paasfile_pln_water_20211103.jsond 数据
 
--- wateraide/src/main/resources/db/water.sql
+++ wateraide/src/main/resources/db/water.sql
@@ -1,45 +1,47 @@
 
-CREATE TABLE IF NOT EXISTS `water_cfg_broker`  (
+CREATE TABLE IF NOT EXISTS `water_cfg_broker` (
   `broker_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '协调器ID',
-  `tag` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '分组标签',
-  `broker` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '协调器',
-  `row_num` bigint(20) NULL DEFAULT 0 COMMENT '累积行数',
-  `row_num_today` bigint(20) NOT NULL DEFAULT 0 COMMENT '今日行数',
-  `row_num_today_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '今日错误行数',
-  `row_num_yesterday` bigint(20) NOT NULL DEFAULT 0 COMMENT '昨天行数',
-  `row_num_yesterday_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '昨天错误行数',
-  `row_num_beforeday` bigint(20) NOT NULL DEFAULT 0 COMMENT '前天行数',
-  `row_num_beforeday_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '前天错误行数',
-  `keep_days` int(11) NOT NULL DEFAULT 15 COMMENT '保留天数',
-  `source` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '数据源',
-  `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `is_enabled` int(11) NOT NULL DEFAULT 1 COMMENT '是否启用',
-  `is_alarm` int(11) NOT NULL DEFAULT 0 COMMENT '是否报警',
+  `tag` varchar(40) NOT NULL DEFAULT '' COMMENT '分组标签',
+  `broker` varchar(100) NOT NULL DEFAULT '' COMMENT '协调器',
+  `row_num` bigint(20) DEFAULT '0' COMMENT '累积行数',
+  `row_num_today` bigint(20) NOT NULL DEFAULT '0' COMMENT '今日行数',
+  `row_num_today_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '今日错误行数',
+  `row_num_yesterday` bigint(20) NOT NULL DEFAULT '0' COMMENT '昨天行数',
+  `row_num_yesterday_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '昨天错误行数',
+  `row_num_beforeday` bigint(20) NOT NULL DEFAULT '0' COMMENT '前天行数',
+  `row_num_beforeday_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '前天错误行数',
+  `keep_days` int(11) NOT NULL DEFAULT '15' COMMENT '保留天数',
+  `source` varchar(100) NOT NULL DEFAULT '' COMMENT '数据源',
+  `note` varchar(255) DEFAULT NULL COMMENT '备注',
+  `is_enabled` int(11) NOT NULL DEFAULT '1' COMMENT '是否启用',
+  `is_alarm` int(11) NOT NULL DEFAULT '0' COMMENT '是否报警',
   `update_fulltime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`broker_id`) USING BTREE,
-  INDEX `IX_tag`(`tag`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'WATER-配置-消息协调器表' ROW_FORMAT = DYNAMIC;
+  UNIQUE KEY `UX_broker` (`broker`) USING BTREE,
+  KEY `IX_tag` (`tag`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='WATER-配置-消息协调器表';
 
-CREATE TABLE IF NOT EXISTS `water_cfg_logger`  (
+CREATE TABLE IF NOT EXISTS   `water_cfg_logger` (
   `logger_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '日志器ID',
-  `tag` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '分组标签',
-  `logger` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '日志器',
-  `row_num` bigint(20) NULL DEFAULT 0 COMMENT '累积行数',
-  `row_num_today` bigint(20) NOT NULL DEFAULT 0 COMMENT '今日行数',
-  `row_num_today_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '今日错误行数',
-  `row_num_yesterday` bigint(20) NOT NULL DEFAULT 0 COMMENT '昨天行数',
-  `row_num_yesterday_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '昨天错误行数',
-  `row_num_beforeday` bigint(20) NOT NULL DEFAULT 0 COMMENT '前天行数',
-  `row_num_beforeday_error` bigint(20) NOT NULL DEFAULT 0 COMMENT '前天错误行数',
-  `keep_days` int(11) NOT NULL DEFAULT 15 COMMENT '保留天数',
-  `source` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '数据源',
-  `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `is_enabled` int(11) NOT NULL DEFAULT 1 COMMENT '是否启用',
-  `is_alarm` int(11) NOT NULL DEFAULT 0 COMMENT '是否报警',
+  `tag` varchar(40) NOT NULL DEFAULT '' COMMENT '分组标签',
+  `logger` varchar(100) NOT NULL DEFAULT '' COMMENT '日志器',
+  `row_num` bigint(20) DEFAULT '0' COMMENT '累积行数',
+  `row_num_today` bigint(20) NOT NULL DEFAULT '0' COMMENT '今日行数',
+  `row_num_today_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '今日错误行数',
+  `row_num_yesterday` bigint(20) NOT NULL DEFAULT '0' COMMENT '昨天行数',
+  `row_num_yesterday_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '昨天错误行数',
+  `row_num_beforeday` bigint(20) NOT NULL DEFAULT '0' COMMENT '前天行数',
+  `row_num_beforeday_error` bigint(20) NOT NULL DEFAULT '0' COMMENT '前天错误行数',
+  `keep_days` int(11) NOT NULL DEFAULT '15' COMMENT '保留天数',
+  `source` varchar(100) NOT NULL DEFAULT '' COMMENT '数据源',
+  `note` varchar(255) DEFAULT NULL COMMENT '备注',
+  `is_enabled` int(11) NOT NULL DEFAULT '1' COMMENT '是否启用',
+  `is_alarm` int(11) NOT NULL DEFAULT '0' COMMENT '是否报警',
   `update_fulltime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`logger_id`) USING BTREE,
-  INDEX `IX_tag`(`tag`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'WATER-配置-日志记录器表' ROW_FORMAT = DYNAMIC;
+  UNIQUE KEY `UX_logger` (`logger`) USING BTREE,
+  KEY `IX_tag` (`tag`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='WATER-配置-日志记录器表';
 
 CREATE TABLE IF NOT EXISTS `water_cfg_properties`  (
   `row_id` int(11) NOT NULL AUTO_INCREMENT,
