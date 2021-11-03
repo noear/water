97141b9f9ac4fa60f460fa95f528e5717491ccb9
noear <noear@live.cn>
2021-11-03 16:25:24 +0800

    2.3.0

--- _db_log2/water.sql
+++ _db_log2/water.sql
@@ -1,8 +1,8 @@
 
 -- 2021-11-03
-ALTER TABLE `water`.`water_cfg_logger`
+ALTER TABLE `water_cfg_logger`
     ADD UNIQUE INDEX `UX_logger`(`logger`) USING BTREE;
 
 
-ALTER TABLE `water`.`water_cfg_broker`
+ALTER TABLE `water_cfg_broker`
     ADD UNIQUE INDEX `UX_broker`(`broker`) USING BTREE;
