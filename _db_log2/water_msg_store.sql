eb320aea74f37fbc753340982d5032e411bf0953
noear <noear@live.cn>
2021-11-06 19:52:43 +0800

    2.3.1

---
+++ _db_log2/water_msg_store.sql
@@ -0,0 +1,12 @@
+
+-- 2021.11.06
+ALTER TABLE `water_msg_distribution`
+    MODIFY COLUMN `log_fulltime` bigint NOT NULL COMMENT '分发时间' AFTER `log_date`;
+
+ALTER TABLE `water_msg_message`
+    MODIFY COLUMN `log_fulltime` bigint NOT NULL COMMENT '记录时间' AFTER `log_date`,
+    MODIFY COLUMN `last_fulltime` bigint NULL COMMENT '最后变更时间' AFTER `last_date`;
+
+ALTER TABLE `water_msg_message_all`
+    MODIFY COLUMN `log_fulltime` bigint NOT NULL COMMENT '记录时间' AFTER `log_date`,
+    MODIFY COLUMN `last_fulltime` bigint NULL COMMENT '最后变更时间' AFTER `last_date`;
