25dc9fabbba4515d5e9b8b02c81c93919a655f41
noear <noear@live.cn>
2021-11-12 16:43:11 +0800

    2.3.1

---
+++ wateradmin/src/main/java/wateradmin/models/water_cfg/GatewayModel.java
@@ -0,0 +1,21 @@
+package wateradmin.models.water_cfg;
+
+import java.util.Date;
+
+/**
+ * @author noear 2021/11/12 created
+ */
+public class GatewayModel {
+    /** 标签 */
+    public String tag;
+    /** 名称 */
+    public String name;
+    /** 代理 */
+    public String proxy;
+    /** 策略 */
+    public String policy;
+    /**  */
+    public int is_enabled;
+    /** 最后更新时间 */
+    public Date update_fulltime;
+}
