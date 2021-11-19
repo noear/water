ac2cf856313deb2943e235596b456b9864c7614c
noear <noear@live.cn>
2021-11-19 21:51:29 +0800

    2.3.3-new

--- _db_log2/water.sql
+++ _db_log2/water.sql
@@ -25,17 +25,33 @@ CREATE TABLE IF NOT EXISTS `water_cfg_gateway` (
 ALTER TABLE `water_cfg_broker`
     CHANGE COLUMN `update_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_alarm`;
 
-ALTER TABLE water_cfg_gateway`
+ALTER TABLE `water_cfg_broker`
+    ADD COLUMN `gmt_create` bigint NULL COMMENT '创建时间' AFTER `is_alarm`;
+
+ALTER TABLE `water_cfg_gateway`
     CHANGE COLUMN `update_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_enabled`;
+
+ALTER TABLE `water_cfg_gateway`
+    ADD COLUMN `gmt_create` bigint NULL COMMENT '创建时间' AFTER `is_enabled`;
+
 
 ALTER TABLE `water_cfg_logger`
     CHANGE COLUMN `update_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_alarm`;
 
+ALTER TABLE `water_cfg_logger`
+    ADD COLUMN `gmt_create` bigint NULL COMMENT '创建时间' AFTER `is_alarm`;
+
 ALTER TABLE `water_cfg_properties`
     CHANGE COLUMN `update_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_enabled`;
 
+ALTER TABLE `water_cfg_properties`
+    ADD COLUMN `gmt_create` bigint NULL COMMENT '创建时间' AFTER `is_enabled`;
+
 ALTER TABLE `water_cfg_whitelist`
     CHANGE COLUMN `update_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_enabled`;
+
+ALTER TABLE `water_cfg_whitelist`
+    ADD COLUMN `gmt_create` bigint NULL COMMENT '创建时间' AFTER `is_enabled`;
 
 ALTER TABLE `water_msg_subscriber`
     CHANGE COLUMN `log_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `check_error_num`;
@@ -45,6 +61,17 @@ ALTER TABLE ` water_msg_subscriber `
 
 ALTER TABLE `water_msg_topic`
     CHANGE COLUMN `create_fulltime` `gmt_create` bigint NULL COMMENT '创建时间' AFTER `alarm_model`;
+
+ALTER TABLE `water_msg_topic`
+    ADD COLUMN `gmt_modified` bigint(20) NULL COMMENT '最后修改时间' AFTER `gmt_create`;
+
+ALTER TABLE `water_ops_project`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `is_enabled`,
+    ADD COLUMN `gmt_modified` bigint(20) NULL COMMENT '最后修改时间' AFTER `gmt_create`;
+
+ALTER TABLE `water_ops_server`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `is_enabled`,
+    ADD COLUMN `gmt_modified` bigint(20) NULL COMMENT '最后修改时间' AFTER `gmt_create`;
 
 ALTER TABLE `water_ops_server_track_bls`
     CHANGE COLUMN `last_updatetime` `gmt_modified` bigint NULL COMMENT '最后修改时间 ' AFTER `traffic_tx`;
@@ -62,17 +89,33 @@ ALTER TABLE `water_reg_consumer`
 ALTER TABLE `water_reg_service`
     MODIFY COLUMN `check_last_time` bigint NOT NULL COMMENT '最后检查时间' AFTER `check_url`;
 
+ALTER TABLE `water_reg_service`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `is_enabled`;
+
+ALTER TABLE `water_reg_service`
+    ADD COLUMN `gmt_modified` bigint(20) NULL COMMENT '最后修改时间' AFTER `gmt_create`;
+
 ALTER TABLE `water_reg_service_runtime`
     MODIFY COLUMN `log_fulltime` bigint NULL AFTER `log_minute`;
 
 ALTER TABLE `water_reg_service_speed`
     CHANGE COLUMN `last_updatetime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `total_num_slow5`;
 
+ALTER TABLE `water_tool_monitor`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `is_enabled`,
+    ADD COLUMN `gmt_modified` bigint(20) NULL COMMENT '最后修改时间' AFTER `gmt_create`;
+
 ALTER TABLE `water_tool_report`
     CHANGE COLUMN `create_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `note`;
 
+ALTER TABLE `water_tool_report`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `note`;
+
 ALTER TABLE `water_tool_synchronous`
     CHANGE COLUMN `last_fulltime` `gmt_modified` bigint NULL COMMENT '最后修改时间' AFTER `is_enabled`;
+
+ALTER TABLE `water_tool_synchronous`
+    ADD COLUMN `gmt_create` bigint(20) NULL COMMENT '创建时间' AFTER `is_enabled`;
 
 ALTER TABLE `water_tool_versions`
     MODIFY COLUMN `log_fulltime` bigint NULL COMMENT '记录完整时间' AFTER `log_date`;
--- _db_log2/water_paas.sql
+++ _db_log2/water_paas.sql
@@ -22,4 +22,39 @@ ALTER  TABLE `paas_file` RENAME TO `luffy_file`;
 ALTER  TABLE `paas_etl` RENAME TO `luffy_etl`;
 
 
+ALTER TABLE `rubber_actor`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `note`;
+
+ALTER TABLE `rubber_block`
+    MODIFY COLUMN `last_updatetime` bigint NULL COMMENT '最后更新时间' AFTER `app_expr`;
+
+ALTER TABLE `rubber_block_item`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `f4`;
+
+ALTER TABLE `rubber_log_request`
+    MODIFY COLUMN `start_fulltime` bigint NULL DEFAULT NULL COMMENT '开始时间' AFTER `note_json`,
+    MODIFY COLUMN `end_fulltime` bigint NULL DEFAULT NULL COMMENT '完成时间' AFTER `start_date`;
+
+ALTER TABLE `rubber_log_request_all`
+    MODIFY COLUMN `start_fulltime` bigint NULL DEFAULT NULL COMMENT '开始时间' AFTER `note_json`,
+    MODIFY COLUMN `end_fulltime` bigint NULL DEFAULT NULL COMMENT '完成时间' AFTER `start_date`;
+
+ALTER TABLE `rubber_model`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `debug_args`;
+
+ALTER TABLE `rubber_model_field`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `note`;
+
+ALTER TABLE `rubber_scheme`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `is_enabled`;
+
+ALTER TABLE `rubber_scheme_node`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `actor_display`;
+
+ALTER TABLE `rubber_scheme_rule`
+    MODIFY COLUMN `last_updatetime` bigint NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `is_enabled`;
+
+ALTER TABLE `rubber_scheme_node_design`
+    ADD COLUMN `last_updatetime` bigint NULL COMMENT '最后更新时间' AFTER `details`;
+
 
