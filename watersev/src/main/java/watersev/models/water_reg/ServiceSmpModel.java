140c8b6e11d6fdd7f1dcd641c9521880f5469ac7
noear <noear@live.cn>
2021-11-19 12:54:46 +0800

    2.3.3

--- watersev/src/main/java/watersev/models/water_reg/ServiceSmpModel.java
+++ watersev/src/main/java/watersev/models/water_reg/ServiceSmpModel.java
@@ -3,6 +3,8 @@ package watersev.models.water_reg;
 import org.noear.weed.GetHandlerEx;
 import org.noear.weed.IBinder;
 
+import java.util.Date;
+
 /**
  * Created by noear on 2017/7/28.
  */
@@ -10,12 +12,18 @@ public class ServiceSmpModel implements IBinder {
     public String name;
     public String address;
     public String meta;
+    public Date check_last_time;
 
     @Override
     public void bind(GetHandlerEx s) {
         name = s.get("name").value("");
         address = s.get("address").value("");
         meta = s.get("meta").value("");
+        check_last_time = s.get("check_last_time").value(null);
+
+        if (check_last_time == null) {
+            check_last_time = new Date();
+        }
     }
 
     @Override
