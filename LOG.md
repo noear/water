2367e3f95bdd50a592f9de5d85f7d1c02abf5d9a
noear <noear@live.cn>
2021-11-20 08:34:47 +0800

    2.3.4

--- LOG.md
+++ LOG.md
@@ -16,6 +16,7 @@
 * 管理后台 /paas/ 开头的路径，改为 /luffy/开头
 * 配置 paas_uri 更名为：faas_uri
 * 监视 paas_file 更名为：luffy_file
+* 服务 waterapi 优化日志格式
 
 
 #### 2.3.3 
--- wateraide/src/test/java/tool/GeneratorJson.java
+++ wateraide/src/test/java/tool/GeneratorJson.java
@@ -39,8 +39,8 @@ public class GeneratorJson {
         //water
 //        buildTableJosn(waterDb, "water_cfg_broker", "water");
 //        buildTableJosn(waterDb, "water_cfg_gateway", "water");
-//        buildTableJosn(waterDb, "water_cfg_logger", "water");
-        buildTableJosn(waterDb, "water_cfg_properties", "water");
+        buildTableJosn(waterDb, "water_cfg_logger", "water");
+//        buildTableJosn(waterDb, "water_cfg_properties", "water");
 //        buildTableJosn(waterDb, "water_cfg_whitelist", "water");
 
 //        buildTableJosn(waterDb, "water_tool_monitor", "water");
