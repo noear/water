6200b2dad3e08e9ad81328a799b6bd7f21485226
noear <noear@live.cn>
2021-11-20 15:04:59 +0800

    2.4.1

--- wateraide/src/test/java/tool/GeneratorJson.java
+++ wateraide/src/test/java/tool/GeneratorJson.java
@@ -39,7 +39,7 @@ public class GeneratorJson {
         //water
 //        buildTableJosn(waterDb, "water_cfg_broker", "water");
 //        buildTableJosn(waterDb, "water_cfg_gateway", "water");
-        buildTableJosn(waterDb, "water_cfg_logger", "water");
+//        buildTableJosn(waterDb, "water_cfg_logger", "water");
 //        buildTableJosn(waterDb, "water_cfg_properties", "water");
 //        buildTableJosn(waterDb, "water_cfg_whitelist", "water");
 
@@ -58,7 +58,7 @@ public class GeneratorJson {
 
 
         //water_paas
-//        buildTableJosn(waterPaasDb, "luffy_file", "water_paas");
+        buildTableJosn(waterPaasDb, "luffy_file", "water_paas");
 
 //        buildTableJosn(waterPaasDb, "rubber_block", "water_paas");
 
