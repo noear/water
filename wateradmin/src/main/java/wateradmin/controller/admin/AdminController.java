7d6b83685f275426323a24611f1a61aae56326fd
noear <noear@live.cn>
2021-11-20 17:35:53 +0800

    2.4.1

--- wateradmin/src/main/java/wateradmin/controller/admin/AdminController.java
+++ wateradmin/src/main/java/wateradmin/controller/admin/AdminController.java
@@ -6,10 +6,10 @@ import org.noear.solon.annotation.Mapping;
 /**
  * @author noear 2021/11/20 created
  */
-@Mapping("/admin/")
 @Controller
 public class AdminController {
-    @Mapping
+
+    @Mapping("/admin")
     public String home(){
         return "{code:1}";
     }
