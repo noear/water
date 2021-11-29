772aa349ad9791ad9c1e3be7484c78e150303cb6
noear <noear@live.cn>
2021-11-29 21:45:13 +0800

    部署说明

--- wateraide/src/test/java/tool/GeneratorJson.java
+++ wateraide/src/test/java/tool/GeneratorJson.java
@@ -40,7 +40,7 @@ public class GeneratorJson {
 //        buildTableJosn(waterDb, "water_cfg_broker", "water");
 //        buildTableJosn(waterDb, "water_cfg_gateway", "water");
 //        buildTableJosn(waterDb, "water_cfg_logger", "water");
-        buildTableJosn(waterDb, "water_cfg_properties", "water");
+//        buildTableJosn(waterDb, "water_cfg_properties", "water");
 //        buildTableJosn(waterDb, "water_cfg_whitelist", "water");
 
 //        buildTableJosn(waterDb, "water_tool_monitor", "water");
@@ -50,7 +50,7 @@ public class GeneratorJson {
         //water_bcf
 //        buildTableJosn(waterDb, "grit_resource", "grit");
 //        buildTableJosn(waterDb, "grit_resource_linked", "grit");
-//        buildTableJosn(waterDb, "grit_subject", "grit");
+        buildTableJosn(waterDb, "grit_subject", "grit");
 //        buildTableJosn(waterDb, "grit_subject_linked", "grit");
 
 
