68a193b03234ea7fd2b7aa44e1555b9e76f7e3aa
noear <noear@live.cn>
2021-12-01 16:36:14 +0800

    2.5.0

---
+++ wateradmin/src/main/java/wateradmin/dso/WeedInit.java
@@ -0,0 +1,111 @@
+package wateradmin.dso;
+
+import lombok.extern.slf4j.Slf4j;
+import org.noear.snack.ONode;
+import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.core.Plugin;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.logging.utils.TagsMDC;
+import org.noear.water.WaterClient;
+import org.noear.water.utils.TextUtils;
+import org.noear.weed.WeedConfig;
+
+/**
+ * @author noear 2021/12/1 created
+ */
+@Slf4j
+@Component
+public class WeedInit implements Plugin {
+    boolean isErrorLogEnable = false;
+    boolean isTrackEnable = false;
+    boolean isDebugMode = false;
+
+    @Override
+    public void start(SolonApp app) {
+        isErrorLogEnable = true;
+        isTrackEnable = Solon.cfg().isDebugMode();
+        isDebugMode = Solon.cfg().isDebugMode();
+
+        initWeedForAdmin();
+
+        WeedConfig.onException((cmd, err) -> {
+            TagsMDC.tag0("weed");
+
+            if (isErrorLogEnable) {
+                if (cmd == null) {
+                    log.error("::Error= {}", err);
+                } else {
+                    log.error("::Sql= {}\n::Args= {}\n\n::Error= {}", cmd.text, ONode.stringify(cmd.paramMap()), err);
+                }
+            } else {
+                if (cmd == null) {
+                    log.debug("::Error= {}", err);
+                } else {
+                    log.debug("::Sql= {}\n::Args= {}\n\n::Error= {}", cmd.text, ONode.stringify(cmd.paramMap()), err);
+                }
+            }
+        });
+    }
+
+    private void initWeedForAdmin() {
+        //admin 项目
+        WeedConfig.onExecuteAft((cmd) -> {
+            if (isDebugMode) {
+
+                log.debug(cmd.text + "\r\n" + ONode.stringify(cmd.paramMap()));
+
+            }
+
+            if (cmd.isLog < 0) {
+                return;
+            }
+
+            Context ctx = Context.current();
+            String user_name = user_name(ctx);
+            int user_puid = user_puid(ctx);
+
+
+            String sqlUp = cmd.text.toUpperCase();
+            String chkUp = "User_Id=? AND Pass_Wd=? AND Is_Disabled=0".toUpperCase();
+
+            if (cmd.timespan() > 2000 || cmd.isLog > 0 || sqlUp.indexOf("INSERT INTO ") >= 0 || sqlUp.indexOf("UPDATE ") >= 0 || sqlUp.indexOf("DELETE ") >= 0 || sqlUp.indexOf(chkUp) >= 0) {
+                WaterClient.Track.track(service_name(), cmd, ctx.userAgent(), ctx.pathNew(), user_puid + "." + user_name, ctx.realIp());
+            }
+
+            if (isTrackEnable) {
+                String tag = cmd.context.schema();
+                if (TextUtils.isEmpty(tag)) {
+                    tag = "sql";
+                }
+
+                CloudClient.metric().addMeter(service_name() + "_sql", tag, cmd.text, cmd.timespan());
+                //WaterClient.Track.track(service_name() + "_sql", tag, cmd.text, cmd.timespan());
+            }
+        });
+    }
+
+    public String service_name() {
+        return Solon.cfg().appName();
+    }
+
+    //用于作行为记录
+    public int user_puid(Context ctx) {
+        if (ctx != null) {
+            String tmp = ctx.attr("user_puid", "0");
+            return Integer.parseInt(tmp);
+        } else {
+            return 0;
+        }
+    }
+
+    public String user_name(Context ctx) {
+        if (ctx != null) {
+            return ctx.attr("user_name", null);
+        } else {
+            return null;
+        }
+    }
+}
