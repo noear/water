ab78673b828e7a18f177190a33a423198fbb9013
noear <noear@live.cn>
2021-12-01 15:46:01 +0800

    部署说明

--- wateradmin/src/main/java/wateradmin/WateradminApp.java
+++ wateradmin/src/main/java/wateradmin/WateradminApp.java
@@ -1,7 +1,10 @@
 package wateradmin;
 
 import org.noear.solon.Solon;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.cloud.model.Instance;
 import org.noear.solon.cloud.utils.http.PreheatUtils;
+import org.noear.solon.core.event.EventBus;
 import org.noear.water.WaterClient;
 import org.noear.water.protocol.ProtocolHub;
 import org.noear.water.protocol.solution.LogSourceFactoryImpl;
@@ -29,7 +32,24 @@ public class WateradminApp {
             ProtocolHub.monitoring = new MonitoringAliyun();
         });
 
+        //尝试注册 gritapi 服务
+        gritApiRegTry();
+
         PreheatUtils.preheat("/run/check/");
         PreheatUtils.preheat("/login");
+    }
+
+    /**
+     * 尝试注册 gritapi 服务
+     * */
+    private static void gritApiRegTry() {
+        try {
+            if (CloudClient.discovery() != null) {
+                Instance instance = new Instance("gritapi", Instance.local().address()).protocol("http");
+                CloudClient.discovery().register("grit", instance);
+            }
+        } catch (Exception e) {
+            EventBus.push(e);
+        }
     }
 }
