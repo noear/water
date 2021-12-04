c473e70a651c87d84903d4bb2b08a27a35fd1774
noear <noear@live.cn>
2021-12-04 14:03:19 +0800

    2.5.1

---
+++ waterapi/src/main/java/waterapi/controller/alarm/CMD_alarm_push.java
@@ -0,0 +1,57 @@
+package waterapi.controller.alarm;
+
+import org.noear.snack.ONode;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.Result;
+import org.noear.solon.validation.annotation.NotEmpty;
+import org.noear.solon.validation.annotation.Whitelist;
+import org.noear.water.protocol.ProtocolHub;
+import org.noear.water.utils.TextUtils;
+import waterapi.Config;
+import waterapi.controller.UapiBase;
+import waterapi.dso.db.DbWaterCfgApi;
+import waterapi.dso.interceptor.Logging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 告警推送
+ *
+ * @author noear
+ * @since 2017.07
+ * Update time 2020.09
+ */
+@Logging
+@Whitelist
+@Controller
+public class CMD_alarm_push extends UapiBase {
+
+    @NotEmpty({"msg", "target"})
+    @Mapping("/alarm/push/")
+    public Result cmd_exec(String msg, String target) throws Exception {
+
+        List<String> list = new ArrayList<String>();
+        for (String str : target.split(",")) {
+            if (str.startsWith("@")) {
+                if (str.length() > 2) {
+                    List<String> mobiles = DbWaterCfgApi.getAlarmMobiles(str.replace("@", "_"));
+
+                    list.addAll(mobiles);
+                }
+            } else {
+                list.add(str);
+            }
+        }
+
+        String rest = ProtocolHub.heihei.push(Config.water_service_name, list, msg);
+
+        if (TextUtils.isNotEmpty(rest)) {
+            return Result.succeed(ONode.load(rest));
+        } else {
+            return Result.failure();
+        }
+    }
+}
