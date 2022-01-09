c20c3989d9a998d58ceb3f852852312e7379b82e
noear <noear@live.cn>
2022-01-09 18:40:34 +0800

    2.5.4

--- xwater/src/test/java/tool/GeneratorJson.java
+++ xwater/src/test/java/tool/GeneratorJson.java
@@ -40,14 +40,14 @@ public class GeneratorJson {
 //        buildTableJosn(waterDb, "water_cfg_broker", "water");
 //        buildTableJosn(waterDb, "water_cfg_gateway", "water");
 //        buildTableJosn(waterDb, "water_cfg_logger", "water");
-//        buildTableJosn(waterDb, "water_cfg_properties", "water");
+        buildTableJosn(waterDb, "water_cfg_properties", "water");
 //        buildTableJosn(waterDb, "water_cfg_whitelist", "water");
 
 //        buildTableJosn(waterDb, "water_tool_monitor", "water");
 //        buildTableJosn(waterDb, "water_tool_report", "water");
 //        buildTableJosn(waterDb, "water_tool_synchronous", "water");
 
-        //water_bcf
+        //grit
 //        buildTableJosn(waterDb, "grit_resource", "grit");
 //        buildTableJosn(waterDb, "grit_resource_linked", "grit");
 //        buildTableJosn(waterDb, "grit_subject", "grit");
@@ -56,7 +56,7 @@ public class GeneratorJson {
 
 
         //water_paas
-        buildTableJosn(waterPaasDb, "luffy_file", "water_paas");
+//        buildTableJosn(waterPaasDb, "luffy_file", "water_paas");
 
 //        buildTableJosn(waterPaasDb, "rubber_block", "water_paas");
 
