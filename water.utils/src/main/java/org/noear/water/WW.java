46d33f6129693734487569898e8af2e4c84dd0b8
noear <noear@live.cn>
2022-01-09 17:45:46 +0800

    2.5.4

--- water.utils/src/main/java/org/noear/water/WW.java
+++ water.utils/src/main/java/org/noear/water/WW.java
@@ -1,7 +1,7 @@
 package org.noear.water;
 
 public class WW {
-    public static final String water_version = "v2.5.3";
+    public static final String water_version = "v2.5.4";
 
     public static final String mime_glog = "water/glog";
     public static final String mime_gzip = "application/x-gzip";
