19634ff0af0359adb1ccde8dc9a7874eefc3fde4
noear <noear@live.cn>
2022-04-02 00:39:36 +0800

    2.5.9

--- wateradmin/src/main/java/wateradmin/controller/mot/SevNodeController.java
+++ wateradmin/src/main/java/wateradmin/controller/mot/SevNodeController.java
@@ -37,11 +37,7 @@ public class SevNodeController extends BaseController {
         TagChecker.filter(tags, m -> m.tag);
 
         //处理空标签
-        tags.forEach(t -> {
-            if (Utils.isEmpty(t.tag)) {
-                t.tag = "_";
-            }
-        });
+        tags.removeIf(m->Utils.isEmpty(m.tag));
 
         tag_name = TagUtil.build(tag_name, tags);
 
