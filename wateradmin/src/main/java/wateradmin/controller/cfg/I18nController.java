080de42a3e59062e5f45c5ec979080c2c2823387
noear <noear@live.cn>
2022-04-07 17:44:22 +0800

    2.5.10

--- wateradmin/src/main/java/wateradmin/controller/cfg/I18nController.java
+++ wateradmin/src/main/java/wateradmin/controller/cfg/I18nController.java
@@ -59,6 +59,12 @@ public class I18nController extends BaseController {
 
         if (Utils.isEmpty(lang) || "default".equals(lang)) {
             lang = ctx.cookie("lang");
+        } else {
+            String lang1 = lang;
+            TagCountsModel lang2 = langs.stream().filter(m -> m.tag.equals(lang1)).findFirst().get();
+            if (lang2 == null) {
+                lang = null;
+            }
         }
 
         if (Utils.isEmpty(lang) || "default".equals(lang)) {
