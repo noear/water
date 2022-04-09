968531bd0ae74d733271b6d6e4b0605b6276a355
noear <noear@live.cn>
2022-04-09 21:59:24 +0800

    2.6.0

--- _bak/etl/core/EtlCount.java
+++
@@ -1,9 +0,0 @@
-package watersev.dso.etl;
-
-public class EtlCount {
-    public int stat_update_ok = 0;
-    public int stat_update_error = 0;
-    public int stat_jump = 0;
-    public int stat_insert_ok = 0;
-    public int stat_insert_error = 0;
-}
