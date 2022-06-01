76392b86fef30d9f130fa6e8b9613caa2f481c23
noear <noear@live.cn>
2022-06-01 08:47:40 +0800

    2.7.1

--- water.utils/src/main/java/org/noear/water/utils/RunUtils.java
+++ water.utils/src/main/java/org/noear/water/utils/RunUtils.java
@@ -6,6 +6,11 @@ import java.util.concurrent.Executors;
 public class RunUtils {
     private static ExecutorService pools = Executors.newCachedThreadPool();
 
+    /**
+     * 异步运行（适合IO密集型）
+     *
+     * //CompletableFuture.runAsync 适合计算密集型
+     * */
     public static void runAsyn(Runnable call) {
         pools.execute(call);
     }
