74a051b11b74836cd1c3c6a99bf4a33b964d6621
noear <noear@live.cn>
2022-06-08 11:51:45 +0800

    2.7.2

---
+++ wateradmin/src/main/java/wateradmin/controller/sev/SevController.java
@@ -0,0 +1,291 @@
+package wateradmin.controller.sev;
+
+import org.noear.snack.ONode;
+import org.noear.solon.Utils;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.auth.annotation.AuthPermissions;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.ModelAndView;
+import org.noear.water.WaterProxy;
+import org.noear.water.utils.HttpUtils;
+import org.noear.water.utils.TextUtils;
+import wateradmin.controller.BaseController;
+import wateradmin.dso.SessionPerms;
+import wateradmin.dso.SettingUtils;
+import wateradmin.dso.TagChecker;
+import wateradmin.dso.TagUtil;
+import wateradmin.dso.db.DbWaterRegApi;
+import wateradmin.models.ScaleType;
+import wateradmin.models.TagCountsModel;
+import wateradmin.models.water_reg.ServiceModel;
+import wateradmin.viewModels.ViewModel;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@Mapping("/sev/")
+public class SevController extends BaseController {
+
+    //服务状态
+    @Mapping("/service")
+    public ModelAndView sev(Context ctx, String tag_name) throws SQLException {
+        List<TagCountsModel> tags = DbWaterRegApi.getServiceTagList();
+
+        //权限过滤
+        TagChecker.filter(tags, m -> m.tag);
+
+        //处理空标签
+        tags.forEach(t -> {
+            if (Utils.isEmpty(t.tag)) {
+                t.tag = "_";
+            }
+        });
+
+        tag_name = TagUtil.build(tag_name, tags);
+
+        viewModel.put("tag_name", tag_name);
+        viewModel.put("tags", tags);
+
+        return view("mot/sev");
+    }
+
+    //服务状态
+    @Mapping("/service/inner")
+    public ModelAndView inner(String tag_name, String name, Integer _state) throws SQLException {
+        if(SettingUtils.serviceScale() == ScaleType.large){
+            List<TagCountsModel> nameList = DbWaterRegApi.getServiceNameList(tag_name);
+
+            if(Utils.isEmpty(name)){
+                if(nameList.size() > 0){
+                    name = nameList.get(0).tag;
+                }
+            }
+
+            viewModel.put("tag_name",tag_name);
+            viewModel.set("tabs", nameList);
+            viewModel.put("tabs_visible",true);
+            viewModel.set("name", name);
+        }else{
+            viewModel.put("tabs_visible",false);
+        }
+
+
+
+        if (_state != null) {
+            viewModel.put("_state", _state);
+            int state = _state;
+            if (state == 0) {
+                _state = 1;
+            } else if (state == 1) {
+                _state = 0;
+            }
+        }
+
+        if (_state == null) {
+            _state = 1;
+        }
+
+        List<ServiceModel> services = DbWaterRegApi.getServices(tag_name, name, _state);
+
+        viewModel.put("tag_name", tag_name);
+        viewModel.put("services", services);
+        viewModel.put("name", name);
+
+        return view("mot/sev_inner");
+    }
+
+    @Mapping("/service/status")
+    public String service_status(String s) throws Exception {
+        if (TextUtils.isEmpty(s)) {
+            return "Not supported";
+        }
+
+        if (s.indexOf("@") < 0 || s.indexOf(":") < 0) {
+            return "Not supported";
+        }
+
+        String addrees = s.split("@")[1];
+
+        try {
+            return WaterProxy.runStatus(addrees);
+        } catch (Throwable ex) {
+            return "The service unsupported";
+        }
+    }
+
+    @Mapping("/service/check")
+    public String service_check(String s) throws Exception {
+        if (TextUtils.isEmpty(s)) {
+            return "Not supported";
+        }
+
+        String[] ss = s.split("@");
+
+        if (ss.length < 3) {
+            return "Not supported";
+        }
+
+        String sip = ss[1];
+        String sid = ss[2];
+
+        ServiceModel sev = DbWaterRegApi.getServiceById(Long.parseLong(sid));
+
+        if (TextUtils.isEmpty(sev.check_url)) {
+            return "Not supported";
+        }
+
+        String url = null;
+        if (sev.check_url.startsWith("/")) {
+            url = "http://" + sip + sev.check_url;
+        } else {
+            url = "http://" + sip + "/" + sev.check_url;
+        }
+
+        try {
+            return HttpUtils.shortHttp(url).get();
+        } catch (Throwable ex) {
+            return "The service unsupported";
+        }
+    }
+
+    //页面自动刷新获取表单数据
+    @Mapping("/service/ajax/service_table")
+    public ModelAndView manageS_table(String tag_name, String name, Integer _state, String _type) throws SQLException {
+        if (_state != null) {
+            viewModel.put("_state", _state);
+            int state = _state;
+            if (state == 0) {
+                _state = 1;
+            } else if (state == 1) {
+                _state = 0;
+            }
+        }
+        if (_state == null)
+            _state = 1;
+        List<ServiceModel> services = DbWaterRegApi.getServices(tag_name, name, _state);
+        viewModel.put("services", services);
+        return view("mot/sev_inner_table");
+    }
+
+    //删除服务
+    @AuthPermissions(SessionPerms.admin)
+    @Mapping("/service/ajax/deleteService")
+    public ViewModel deleteServiceById(Integer service_id) throws SQLException {
+        ServiceModel sev = DbWaterRegApi.getServiceById(service_id);
+        boolean result = DbWaterRegApi.deleteServiceById(service_id);
+
+        //删除消费者记录
+        DbWaterRegApi.delConsumer(sev.address);
+
+        if (result) {
+            viewModel.code(1, "删除成功！");
+        } else {
+            viewModel.code(0, "删除失败！");
+        }
+
+        return viewModel;
+    }
+
+    //启用 | 禁用 服务
+    @AuthPermissions(SessionPerms.admin)
+    @Mapping("/service/ajax/disable")
+    public ViewModel disable(Integer service_id, Integer is_enabled) throws SQLException {
+
+        boolean result = DbWaterRegApi.disableService(service_id, is_enabled);
+        if (result) {
+            viewModel.code(1, "操作成功！");
+        } else {
+            viewModel.code(0, "操作失败！");
+        }
+
+        return viewModel;
+    }
+
+    //服务状态
+    @Mapping("/service/edit")
+    public ModelAndView service_edit(Integer service_id) throws SQLException {
+        ServiceModel model = new ServiceModel();
+        if (service_id != null) {
+            model = DbWaterRegApi.getServiceById(service_id);
+        }
+
+        viewModel.put("model", model);
+        viewModel.put("service_id", service_id);
+
+        return view("mot/sev_edit");
+    }
+
+    @AuthPermissions(SessionPerms.admin)
+    @Mapping("/service/edit/ajax/save")
+    public ViewModel service_edit_ajax_save(Integer service_id, String tag, String name, String address, String note, Integer check_type, String check_url) throws SQLException {
+        boolean result = DbWaterRegApi.udpService(service_id, tag, name, address, note, check_type, check_url);
+
+        if (result) {
+            viewModel.code(1, "保存成功！");
+        } else {
+            viewModel.code(0, "保存失败！");
+        }
+
+        return viewModel;
+    }
+
+
+    //性能监控图标统计
+    // todo: 未完成
+    @Mapping("/service/charts")
+    public ModelAndView speedCharts(String key) throws SQLException {
+        if (key == null) {
+            key = "";
+        }
+
+        ServiceModel sev = DbWaterRegApi.getServiceByKey(key);
+
+        Map<String, List> speedReqTate = DbWaterRegApi.getChartsForDate(key, "memory_used"); //total_num
+        Map<String, List> speeds = DbWaterRegApi.getChartsForMonth(key);
+        viewModel.put("speedReqTate", ONode.stringify(speedReqTate));
+        viewModel.put("speeds", ONode.stringify(speeds));
+        viewModel.put("key", key);
+        viewModel.put("service", sev.name);
+        viewModel.put("address", sev.getAddress());
+
+        return view("mot/sev_charts");
+    }
+
+    // todo: 未完成
+    @Mapping("/service/charts/ajax/reqtate")
+    public ViewModel speedCharts_reqtate(String key, Integer type) throws SQLException {
+        String valField = "memory_used";
+        if (type == null) {
+            type = 0;
+        }
+        switch (type) {
+            case 0:
+                valField = "memory_used";
+                break;
+            case 1:
+                valField = "memory_total";
+                break;
+            case 2:
+                valField = "memory_max";
+                break;
+            case 3:
+                valField = "thread_peak_count";
+                break;
+            case 4:
+                valField = "thread_count";
+                break;
+            case 5:
+                valField = "thread_daemon_count";
+                break;
+        }
+
+        Map<String, List> speedReqTate = DbWaterRegApi.getChartsForDate(key, valField);
+        viewModel.put("speedReqTate", speedReqTate);
+        viewModel.put("key", key);
+        viewModel.put("typeName", valField);
+        return viewModel;
+    }
+}
---
+++ wateradmin/src/main/resources/WEB-INF/view/sev/sev_inner.ftl
@@ -0,0 +1,234 @@
+<!DOCTYPE HTML>
+<html class="frm10">
+<head>
+    <title>${app} - 服务监视</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8 "/>
+    <link rel="stylesheet" href="${css}/main.css"/>
+    <script src="/_session/domain.js"></script>
+    <script src="${js}/lib.js"></script>
+    <script src="${js}/layer/layer.js"></script>
+    <style>
+        /* tooltip */
+        #tooltip{
+            position:absolute;
+            border:1px solid #aaa;
+            background:#eee;
+            padding:1px;
+            color:#333;
+            display:none;
+            font-size: small;
+        }
+        .tabs{padding-bottom: 10px;}
+        .tabs a.btn{margin: 0 5px 5px 5px!important;}
+    </style>
+    <script>
+        function deleteService(service_id) {
+            top.layer.confirm('确定删除', {
+                btn: ['确定','取消'] //按钮
+            }, function(){
+                $.ajax({
+                    type:"POST",
+                    url:"/sev/service/ajax/deleteService",
+                    data:{"service_id":service_id},
+                    success:function(data){
+                        top.layer.msg('操作成功');
+                        if ( $('#fresh').text() == '开启自动刷新') {
+                            location.reload();
+                        }
+                    }
+                });
+                top.layer.close(top.layer.index);
+            });
+        };
+
+        function disableService(service_id,type) {
+            var text = "启用";
+            if (type == 0) {
+                text = "禁用";
+            }
+            top.layer.confirm('确定'+text, {
+                btn: ['确定','取消'] //按钮
+            }, function(){
+                $.ajax({
+                    type:"POST",
+                    url:"/sev/service/ajax/disable",
+                    data:{"service_id":service_id,"is_enabled":type},
+                    success:function(data){
+                        top.layer.msg(data.msg);
+                        if ( $('#fresh').text() == '开启自动刷新') {
+                            location.reload();
+                        }
+                    }
+                });
+                top.layer.close(top.layer.index);
+            });
+        };
+
+        function autofresh() {
+            if ( $('#fresh').text() == '关闭自动刷新') {
+                $('#fresh').text('开启自动刷新');
+                location.reload();
+            } else {
+                $('#fresh').text('关闭自动刷新');
+                setInterval(function(){
+                    freshData();
+                }, 3000);
+            }
+
+        };
+
+        function freshData() {
+            var name = $('#name').val();
+            var _state = 0;
+            <#if _state??>
+            var _state = ${_state};
+            </#if>
+
+            $.get('/sev/service/ajax/service_table?tag_name=${tag_name!}&name='+name+'&_state='+_state,function (rst) {
+                $('datagrid').empty();
+                $('datagrid').html(rst);
+            })
+        };
+
+        $(function(){
+            var x = 10;
+            var y = 20;
+            $("tr[title]").mouseover(function(e){
+                this.myTitle = this.title.replace(/；/g,"<br/>")
+                this.title = "";
+                var tooltip = "<div id='tooltip'>"+ this.myTitle +"<\/div>"; //创建 div 元素 文字提示
+                $("body").append(tooltip);    //把它追加到文档中
+                $("#tooltip").css({
+                    "top": (e.pageY+y) + "px",
+                    "left": (e.pageX+x)  + "px"
+                }).show();      //设置x坐标和y坐标，并且显示
+            }).mouseout(function(){
+                this.title = this.myTitle;
+                $("#tooltip").remove();   //移除
+            }).mousemove(function(e){
+                $("#tooltip").css({
+                    "top": (e.pageY+y) + "px",
+                    "left": (e.pageX+x)  + "px"
+                });
+            });
+        });
+    </script>
+</head>
+<body>
+<main>
+    <#if tabs_visible!false>
+    <div class="tabs">
+        <tabbar>
+            <#list tabs as m>
+                <#if m.tag == name>
+                    <a id="e${m.tag}" class="btn sel">${m.tag!}</a>
+                <#else>
+                    <a id="e${m.tag}" class="btn" href="/sev/service/inner?tag_name=${tag_name!}&name=${m.tag!}">${m.tag!}</a>
+                </#if>
+            </#list>
+        </tabbar>
+    </div>
+    </#if>
+
+    <form>
+        <toolbar>
+            <flex>
+                <left class="col-4">
+                    <button onclick="autofresh();" class="w100"  type="button" id="fresh">开启自动刷新</button>
+                    <#if is_operator == 1>
+                        <a class="btn edit mar10-l" href="/sev/service/edit">添加</a>
+                    </#if>
+                </left>
+                <middle class="col-4 center">
+                    <input type="text" class="w200" name="name" placeholder="名称" id="name" value="${name!}"/>
+                    <button type="submit">查询</button>
+                </middle>
+                <right class="col-4">
+                    <@stateselector items="启用,未启用"/>
+                </right>
+            </flex>
+        </toolbar>
+    </form>
+    <datagrid class="list">
+        <table>
+            <thead>
+            <tr>
+                <td width="140px" class="left">名称</td>
+                <td class="left">地址</td>
+                <td width="50px">检测<br/>类型</td>
+                <td>检测路径</td>
+                <td width="120px">检测情况</td>
+                <#if is_admin == 1>
+                    <td width="170px">操作</td>
+                <#else>
+                    <td width="80px">操作</td>
+                </#if>
+            </tr>
+            </thead>
+            <tbody id="tbody" >
+            <#list services as m>
+                <#if m.check_last_state == 1>
+                <tr style="color: red" title="${m.code_location!}">
+                <#else>
+                    <tr title="${m.code_location!}">
+                </#if>
+                <td class="left">${m.name}</td>
+                <td class="left break">
+                    <#if m.check_type == 0>
+                        <a href="/sev/service/status?s=${m.name}@${m.address}" target="_blank">
+                            ${m.address}
+                            <#if m.note?default("")?length gt 0>
+                                - ${m.note}
+                            </#if>
+                        </a>
+                    <#else>
+                        ${m.address}
+                        <#if m.note?default("")?length gt 0>
+                            - ${m.note}
+                        </#if>
+                    </#if>
+                </td>
+                <td>
+                    ${(m.check_type == 0)?string("被动","主动")}
+                </td>
+                <td class="left">
+                    <#if m.check_url?default('')?length gt 0 >
+                        <a href="/sev/service/check?s=${m.name}@${m.address}@${m.service_id}" target="_blank">
+                            ${m.check_url!}
+                        </a>
+                    </#if>
+                </td>
+
+                <td style='${m.isAlarm()?string("color:red","")}'>
+                    ${(m.check_last_time?string('HH:mm:ss'))!}
+                    <#if m.check_last_state == 0>
+                        - ok
+                    <#else>
+                        - no
+                    </#if>
+                </td>
+
+                <td class="op">
+                    <#if is_admin == 1>
+                        <a class="t2" onclick="deleteService('${m.service_id}')">删除</a> |
+                        <#if m.is_enabled == 1>
+                            <a class="t2" onclick="disableService('${m.service_id}',0)">禁用</a>
+                        </#if>
+                        <#if m.is_enabled == 0>
+                            <a class="t2" onclick="disableService('${m.service_id}',1)">启用</a>
+                        </#if>
+                    </#if>
+                    |
+                    <a href="/log/query/inner?tag_name=water&logger=water_log_sev&level=0&tagx=sevchk@${m.address}" class="t2">日志</a>
+                    |
+                    <a href="/sev/service/charts?key=${m.key}" class="t2">监控</a>
+                </td>
+                </tr>
+            </#list>
+            </tbody>
+        </table>
+    </datagrid>
+</main>
+</body>
+</html>
