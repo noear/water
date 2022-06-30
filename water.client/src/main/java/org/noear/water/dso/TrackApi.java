b3cc7d8492258c71c77816826b22342836f3567b
noear <noear@live.cn>
2022-06-30 22:14:49 +0800

    2.8.0

--- water.client/src/main/java/org/noear/water/dso/TrackApi.java
+++ water.client/src/main/java/org/noear/water/dso/TrackApi.java
@@ -1,17 +1,13 @@
 package org.noear.water.dso;
 
-import org.noear.redisx.RedisClient;
 import org.noear.snack.ONode;
 import org.noear.water.WW;
 import org.noear.water.WaterAddress;
-import org.noear.water.WaterClient;
 import org.noear.water.WaterSetting;
-import org.noear.water.model.LogM;
 import org.noear.water.track.TrackEventGather;
 import org.noear.water.track.TrackNames;
 import org.noear.water.utils.GzipUtils;
 import org.noear.water.utils.TextUtils;
-import org.noear.weed.Command;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,16 +25,7 @@ public class TrackApi {
         apiCaller = new ApiCaller(WaterAddress.getDefApiUrl());
     }
 
-    //db:5
-    public static RedisClient rd_track;
-    public static RedisClient rd_track_md5;
 
-    static {
-        rd_track = WaterSetting.redis_track_cfg().getRd(5);
-        rd_track_md5 = WaterSetting.redis_track_cfg().getRd(6);
-        //TrackPipeline.singleton().bind(rd_track);
-        TrackNames.singleton().bind(rd_track_md5);
-    }
 
     public String getNameMd5(String name) {
         return TrackNames.singleton().getNameMd5(name);
@@ -48,6 +35,18 @@ public class TrackApi {
         return TrackNames.singleton().getName(nameMd5);
     }
 
+
+    public void trackCount(String service, String tag, String name, long count) {
+        TrackPipeline.singleton().appendCount(service, tag, name, count);
+    }
+
+    public void trackCount(String service, String tag, String name, long count, long count5) {
+        TrackPipeline.singleton().appendCount(service, tag, name, count, count5);
+    }
+
+    public void trackCount(String service, String tag, String name, long count, long count1, long count2, long count5) {
+        TrackPipeline.singleton().appendCount(service, tag, name, count, count1, count2, count5);
+    }
 
     /**
      * 跟踪请求性能
@@ -68,111 +67,6 @@ public class TrackApi {
             TrackPipeline.singleton().appendFrom(service, _from, timespan);
         }
     }
-
-    /**
-     * 跟踪SQL命令性能
-     */
-    @Deprecated
-    public void track(String service, Command cmd, long thresholdValue) {
-        trackOfPerformance(service, cmd, thresholdValue);
-    }
-
-    /**
-     * 跟踪SQL性能
-     *
-     * @param service        服务名
-     * @param thresholdValue 阈值
-     */
-    public void trackOfPerformance(String service, Command cmd, long thresholdValue) {
-        long timespan = cmd.timespan();
-
-        if (timespan > thresholdValue) {
-            track0(service, cmd, null, null, null, WaterClient.localHost());
-        }
-    }
-
-
-    @Deprecated
-    public void track(String service, Command cmd, String ua, String path, String operator, String operator_ip) {
-        trackOfBehavior(service, cmd, ua, path, operator, operator_ip);
-    }
-
-    /**
-     * 跟踪SQL行为
-     *
-     * @param service     服务名
-     * @param ua          ua
-     * @param path        请求路径
-     * @param operator    操作人
-     * @param operator_ip 操作IP
-     */
-    public void trackOfBehavior(String service, Command cmd, String ua, String path, String operator, String operator_ip) {
-        track0(service, cmd, ua, path, operator, operator_ip);
-    }
-
-    /**
-     * 跟踪SQL命令性能
-     */
-    private void track0(String service, Command cmd, String ua, String path, String operator, String operator_ip) {
-        long interval = cmd.timespan();
-        String trace_id = WaterClient.waterTraceId();
-
-        WaterSetting.pools.submit(() -> {
-            track0Do(service, trace_id, cmd, interval, ua, path, operator, operator_ip);
-        });
-    }
-
-    private void track0Do(String service, String trace_id, Command cmd, long interval, String ua, String path, String operator, String operator_ip) {
-        int seconds = (int) (interval / 1000);
-        String schema = cmd.context.schema();
-
-        String sqlUp = cmd.text.toUpperCase();
-
-        String method = "OTHER";
-        if (sqlUp.indexOf("SELECT ") >= 0) {
-            method = "SELECT";
-        } else if (sqlUp.indexOf("UPDATE ") >= 0) {
-            method = "UPDATE";
-        } else if (sqlUp.indexOf("DELETE ") >= 0) {
-            method = "DELETE";
-        } else if (sqlUp.indexOf("INSERT INTO ") >= 0) {
-            method = "INSERT";
-        }
-
-        LogM logM = new LogM();
-
-        if (path == null) {
-            logM.logger = WW.logger_water_log_sql_p;
-        } else {
-            logM.logger = WW.logger_water_log_sql_b;
-        }
-
-        logM.trace_id = trace_id;
-
-        logM.level = 1;
-
-        logM.tag = String.valueOf(seconds);//留空
-        logM.tag1 = path; //秒数
-        logM.tag2 = operator;
-        logM.tag3 = method;
-        logM.tag4 = "";
-
-        logM.weight = interval; //=tag5, 毫秒数
-
-        logM.group = schema; //=tag6
-        logM.service = service;//=tag7
-
-        StringBuilder content = new StringBuilder();
-
-        content.append(schema).append("::").append(cmd.text);
-        content.append("<n-l>$$$").append(ONode.stringify(cmd.paramMap())).append("</n-l>");
-
-        logM.content = content.toString();
-        logM.from = operator_ip;
-
-        WaterClient.Log.append(logM);
-    }
-
     public void appendAll(TrackEventGather gather, boolean async) {
         if (async) {
             WaterSetting.pools.submit(() -> {
---
+++ water.client/src/main/java/org/noear/water/utils/BehaviorUtils.java
@@ -0,0 +1,111 @@
+package org.noear.water.utils;
+
+import org.noear.redisx.RedisClient;
+import org.noear.snack.ONode;
+import org.noear.water.WW;
+import org.noear.water.WaterClient;
+import org.noear.water.WaterSetting;
+import org.noear.water.model.LogM;
+import org.noear.water.track.TrackNames;
+import org.noear.weed.Command;
+
+/**
+ * @author noear 2022/6/30 created
+ */
+public class BehaviorUtils {
+    public static RedisClient rd_track_md5;
+
+    static {
+        rd_track_md5 = WaterSetting.redis_track_cfg().getRd(6);
+        TrackNames.singleton().bind(rd_track_md5);
+    }
+
+
+    /**
+     * 跟踪SQL性能
+     *
+     * @param service        服务名
+     * @param thresholdValue 阈值
+     */
+    public static void trackOfPerformance(String service, Command cmd, long thresholdValue) {
+        long timespan = cmd.timespan();
+
+        if (timespan > thresholdValue) {
+            track0(service, cmd, null, null, null, WaterClient.localHost());
+        }
+    }
+
+    /**
+     * 跟踪SQL行为
+     *
+     * @param service     服务名
+     * @param ua          ua
+     * @param path        请求路径
+     * @param operator    操作人
+     * @param operator_ip 操作IP
+     */
+    public static void trackOfBehavior(String service, Command cmd, String ua, String path, String operator, String operator_ip) {
+        track0(service, cmd, ua, path, operator, operator_ip);
+    }
+
+    /**
+     * 跟踪SQL命令性能
+     */
+    private static void track0(String service, Command cmd, String ua, String path, String operator, String operator_ip) {
+        long interval = cmd.timespan();
+        String trace_id = WaterClient.waterTraceId();
+
+        track0Do(service, trace_id, cmd, interval, ua, path, operator, operator_ip);
+    }
+
+    private static void track0Do(String service, String trace_id, Command cmd, long interval, String ua, String path, String operator, String operator_ip) {
+        int seconds = (int) (interval / 1000);
+        String schema = cmd.context.schema();
+
+        String sqlUp = cmd.text.toUpperCase();
+
+        String method = "OTHER";
+        if (sqlUp.indexOf("SELECT ") >= 0) {
+            method = "SELECT";
+        } else if (sqlUp.indexOf("UPDATE ") >= 0) {
+            method = "UPDATE";
+        } else if (sqlUp.indexOf("DELETE ") >= 0) {
+            method = "DELETE";
+        } else if (sqlUp.indexOf("INSERT INTO ") >= 0) {
+            method = "INSERT";
+        }
+
+        LogM logM = new LogM();
+
+        if (path == null) {
+            logM.logger = WW.logger_water_log_sql_p;
+        } else {
+            logM.logger = WW.logger_water_log_sql_b;
+        }
+
+        logM.trace_id = trace_id;
+
+        logM.level = 1;
+
+        logM.tag = String.valueOf(seconds);//留空
+        logM.tag1 = path; //秒数
+        logM.tag2 = operator;
+        logM.tag3 = method;
+        logM.tag4 = "";
+
+        logM.weight = interval; //=tag5, 毫秒数
+
+        logM.group = schema; //=tag6
+        logM.service = service;//=tag7
+
+        StringBuilder content = new StringBuilder();
+
+        content.append(schema).append("::").append(cmd.text);
+        content.append("<n-l>$$$").append(ONode.stringify(cmd.paramMap())).append("</n-l>");
+
+        logM.content = content.toString();
+        logM.from = operator_ip;
+
+        WaterClient.Log.append(logM);
+    }
+}
