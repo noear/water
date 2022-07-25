d5a835b33b8aeddef2c3684750ab3a6c5077077c
noear <noear@live.cn>
2022-07-25 15:30:20 +0800

    2.8.1

--- xwater/src/main/resources/db/water.sql
+++ xwater/src/main/resources/db/water.sql
@@ -361,6 +361,30 @@ CREATE TABLE IF NOT EXISTS `water_reg_service_speed_hour`  (
 ) ENGINE = InnoDB  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'WATER-注册-服务性能记录表-按时' ROW_FORMAT = DYNAMIC;
 
 
+CREATE TABLE IF NOT EXISTS  `water_tool_certification` (
+    `certification_id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `tag` varchar(40) NOT NULL DEFAULT '' COMMENT '分类标签',
+    `key` varchar(40) NOT NULL COMMENT '唯一key',
+    `url` varchar(500) NOT NULL COMMENT '地址',
+    `note` varchar(255) DEFAULT NULL COMMENT '备注',
+    `time_of_start` timestamp NULL DEFAULT NULL COMMENT '生效时间',
+    `time_of_end` timestamp NULL DEFAULT NULL COMMENT '失效时间',
+    `state` int(11) NOT NULL DEFAULT '0' COMMENT '0:待检查；1检查中',
+    `check_error_num` int(11) NOT NULL DEFAULT '0' COMMENT '检测异常数量',
+    `check_interval` int(11) NOT NULL DEFAULT '0' COMMENT '检测间隔时间(s)',
+    `check_last_time` bigint(20) NOT NULL COMMENT '最后检查时间',
+    `check_last_state` int(11) NOT NULL DEFAULT '0' COMMENT '最后检查状态（0：OK；1：error）',
+    `check_last_note` varchar(255) DEFAULT NULL COMMENT '最后检查描述',
+    `is_enabled` int(11) NOT NULL DEFAULT '1' COMMENT '是否为已启用',
+    `gmt_create` bigint(20) DEFAULT NULL COMMENT '创建时间',
+    `gmt_modified` bigint(20) DEFAULT NULL COMMENT '最后修改时间',
+    PRIMARY KEY (`certification_id`) USING BTREE,
+    UNIQUE KEY `IX_key` (`key`) USING BTREE,
+    KEY `IX_tag` (`tag`) USING BTREE
+) ENGINE=InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='WATER-工具-证书监视';
+
+
+
 CREATE TABLE IF NOT EXISTS  `water_tool_detection` (
     `detection_id` bigint(20) NOT NULL AUTO_INCREMENT,
     `tag` varchar(40) NOT NULL DEFAULT '' COMMENT '分类标签',
@@ -379,7 +403,8 @@ CREATE TABLE IF NOT EXISTS  `water_tool_detection` (
     `gmt_modified` bigint(20) DEFAULT NULL COMMENT '最后修改时间',
     PRIMARY KEY (`detection_id`) USING BTREE,
     UNIQUE KEY `IX_key` (`key`) USING BTREE,
-    KEY `IX_tag` (`tag`) USING BTREE
+    KEY `IX_tag` (`tag`) USING BTREE,
+    KEY `IX_time_of_end` (`time_of_end`)
 ) ENGINE=InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT='WATER-工具-应用监视' ROW_FORMAT = DYNAMIC;
 
 
