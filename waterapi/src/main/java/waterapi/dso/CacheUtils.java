efd299f9cb39ca2717afe96d1e8a3338c3260eb0
noear <noear@live.cn>
2022-10-16 16:49:23 +0800

    2.9.0

--- waterapi/src/main/java/waterapi/dso/CacheUtils.java
+++ waterapi/src/main/java/waterapi/dso/CacheUtils.java
@@ -9,5 +9,5 @@ public class CacheUtils {
     private static ICacheServiceEx c1 = new LocalCache("data", 60 * 10);
     private static ICacheServiceEx c2 = Config.cfg("water_cache").getCh("WATERAPI", 60 * 10);
 
-    public static ICacheServiceEx data = new SecondCache(c1, c2);
+    public static ICacheServiceEx data = c1;//new SecondCache(c1, c2);
 }
