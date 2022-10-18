de1f2d858e553d3ec8963d0eddf0febc653ecf7f
noear <noear@live.cn>
2022-10-18 10:40:34 +0800

    2.9.0

--- waterapi/src/main/java/waterapi/dso/CacheUtils.java
+++ waterapi/src/main/java/waterapi/dso/CacheUtils.java
@@ -7,7 +7,7 @@ import waterapi.Config;
 
 public class CacheUtils {
     private static ICacheServiceEx c1 = new LocalCache("data", 60 * 10);
-    private static ICacheServiceEx c2 = Config.cfg("water_cache").getCh("WATERAPI", 60 * 10);
+    private static ICacheServiceEx c2 = Config.cfg("water_cache").getCh("WATERAPI2", 60 * 10);
 
-    public static ICacheServiceEx data = c1;//new SecondCache(c1, c2);//2022-10
+    public static ICacheServiceEx data = new SecondCache(c1, c2);//2022-10
 }
--- watersev/src/main/java/watersev/Config.java
+++ watersev/src/main/java/watersev/Config.java
@@ -14,8 +14,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Config {
-    public static final ICacheServiceEx cache_file = new LocalCache();
-    public static final ICacheServiceEx cache_data = new LocalCache().nameSet("cache_data");
+    public static final ICacheServiceEx cache_file;// = new LocalCache();
+    public static final ICacheServiceEx cache_data; //= new LocalCache().nameSet("cache_data");
     //public static final ICacheServiceEx cache_data;
 
     public static final DbContext water;
@@ -38,7 +38,8 @@ public class Config {
         water = DsCacheUtils.getDb(cfg(WW.water).value, true);
         water_paas = DsCacheUtils.getDb(cfg(WW.water_paas).value, true, water);
 
-        //cache_data = cfg("water_cache").getCh().nameSet("cache_data"); //2022-10
+        cache_file = new LocalCache();
+        cache_data = cfg("water_cache").getCh("WATERSEV2", 60 * 5).nameSet("cache_data"); //2022-10
     }
 
     public static void tryInit() {
