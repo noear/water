f9338abd71faa54f623cb02b3ee7af8f9f4eecb6
noear <noear@live.cn>
2023-05-05 04:00:38 +0800

    mysql 升为 8.0.32

--- wateradmin/src/main/java/wateradmin/dso/aot/RuntimeNativeRegistrarImpl.java
+++ wateradmin/src/main/java/wateradmin/dso/aot/RuntimeNativeRegistrarImpl.java
@@ -1,8 +1,5 @@
 package wateradmin.dso.aot;
 
-import com.mysql.jdbc.*;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
-import com.mysql.jdbc.log.StandardLogger;
 import com.zaxxer.hikari.HikariConfig;
 import io.jsonwebtoken.impl.DefaultJwtBuilder;
 import io.jsonwebtoken.impl.DefaultJwtParserBuilder;
@@ -26,20 +23,6 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerReflection(LuffyFileModel.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
                 MemberCategory.INVOKE_DECLARED_METHODS);
 
-        metadata.registerReflection(PreparedStatement.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(ResultSetImpl.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(ResultSetMetaData.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(JDBC4ResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC4UpdatableResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42ResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42UpdatableResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42PreparedStatement.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(MySQLIntegrityConstraintViolationException.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(StandardSocketFactory.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(StandardLogger.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
 
         metadata.registerReflection(DefaultJwtBuilder.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
         metadata.registerReflection(DefaultJwtParserBuilder.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
--- waterapi/src/main/java/waterapi/dso/aot/RuntimeNativeRegistrarImpl.java
+++ waterapi/src/main/java/waterapi/dso/aot/RuntimeNativeRegistrarImpl.java
@@ -1,8 +1,5 @@
 package waterapi.dso.aot;
 
-import com.mysql.jdbc.*;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
-import com.mysql.jdbc.log.StandardLogger;
 import com.zaxxer.hikari.HikariConfig;
 import org.noear.solon.aot.RuntimeNativeMetadata;
 import org.noear.solon.aot.RuntimeNativeRegistrar;
@@ -17,22 +14,6 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
     @Override
     public void register(AopContext context, RuntimeNativeMetadata metadata) {
         metadata.registerResourceInclude("com.mysql.jdbc.LocalizedErrorMessages.properties");
-
-
-        metadata.registerReflection(PreparedStatement.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(ResultSetImpl.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(ResultSetMetaData.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(JDBC4ResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC4UpdatableResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42ResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42UpdatableResultSet.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(JDBC42PreparedStatement.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(MySQLIntegrityConstraintViolationException.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-
-        metadata.registerReflection(StandardSocketFactory.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        metadata.registerReflection(StandardLogger.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
 
         metadata.registerReflection(HikariConfig.class, MemberCategory.INVOKE_DECLARED_METHODS);
     }
